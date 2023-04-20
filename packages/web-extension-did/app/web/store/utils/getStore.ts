065f0636a8aba9d2e6861e63a1acd2db1473be48
sarah-portkey <sarah@portkey.finance>
2023-04-20 16:02:38 +0800

    fix: logout logic

--- packages/web-extension-did/app/web/store/utils/getStore.ts
+++ packages/web-extension-did/app/web/store/utils/getStore.ts
@@ -1,5 +1,18 @@
+import { DefaultChainId } from '@portkey-wallet/constants/constants-ca/network';
 import { store } from 'store/Provider/store';
 
 export const getStoreState = () => {
   return store.getState();
+};
+
+export const getWallet = () => getStoreState().wallet;
+
+export const getWalletInfo = () => getWallet()?.walletInfo;
+
+export const isCurrentCaHash = (caHash: string) => {
+  const wallet = getWallet();
+  const { walletInfo, currentNetwork } = wallet || {};
+  const caInfo: any = walletInfo?.caInfo?.[currentNetwork];
+  const originChainId = wallet.originChainId || caInfo?.originChainId;
+  return caInfo?.[originChainId || DefaultChainId]?.caHash === caHash;
 };

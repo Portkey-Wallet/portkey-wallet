8927d7caf6aadc6fa973859624c22a8e14d0a524
ykx <yangkexin@portkey.finance>
2023-04-24 14:40:02 +0800

    test: 💍 add chainInfo mock data

---
+++ test/data/chainInfo.ts
@@ -0,0 +1,19 @@
+/**
+ * @for useGetChainInfo
+ */
+export const AELFChainInfo = {
+  chainId: 'AELF',
+  chainName: 'AELF',
+  endPoint: 'http://192.168.66.61:8000',
+  explorerUrl: 'http://192.168.66.61:8000',
+  caContractAddress: '2nyC8hqq3pGnRu8gJzCsTaxXB6snfGxmL2viimKXgEfYWGtjEh',
+  defaultToken: {
+    name: 'AELF',
+    address: 'JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE',
+    imageUrl: 'https://portkey-did.s3.ap-northeast-1.amazonaws.com/img/aelf_token_logo.png',
+    symbol: 'ELF',
+    decimals: '8',
+  },
+  lastModifyTime: '2023-03-29T08:58:25.5132315Z',
+  id: 'AELF',
+};

b2e4297f369cbcf22eb302effa0a0dce6ebdb628
kong lq <qin@portkey.finance>
2023-04-26 14:57:23 +0800

    feat: check verify api add

--- packages/mobile-app-did/js/components/VerifyHumanMachine/index.tsx
+++ packages/mobile-app-did/js/components/VerifyHumanMachine/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import RecaptchaModal from 'components/RecaptchaModal';
 import OverlayModal from '../OverlayModal';
 import { screenWidth, screenHeight } from '@portkey-wallet/utils/mobile/device';
+import { request } from '@portkey-wallet/api/api-did';
 
-async function verifyHumanMachine(language: any, needVerifyFunc?: () => Promise<boolean>) {
+async function checkNeedVerify() {
+  const req = await request.verify.checkGoogleRecaptcha({});
+  return req as boolean;
+}
+async function verifyHumanMachine(language: any, needVerifyFunc: () => Promise<boolean> = checkNeedVerify) {
   let needVerify = false;
   if (needVerifyFunc) {
     needVerify = await needVerifyFunc();

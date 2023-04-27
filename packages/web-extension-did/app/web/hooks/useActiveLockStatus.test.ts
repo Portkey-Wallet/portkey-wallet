66c7939e81323952498bafaba65e6be838adb656
ykx <yangkexin@portkey.finance>
2023-04-27 14:29:10 +0800

    test: 💍 add useActiveLockStatus ut

--- packages/web-extension-did/app/web/hooks/useActiveLockStatus.test.ts
+++ packages/web-extension-did/app/web/hooks/useActiveLockStatus.test.ts
@@ -1,14 +1,21 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useActiveLockStatus } from './useActiveLockStatus';
-import { useActiveLockStatusAction } from 'utils/lib/serviceWorkerAction';
+import * as serviceWorkerAction from 'utils/lib/serviceWorkerAction';
 import { useCommonState } from 'store/Provider/hooks';
 
-jest.mock('utils/lib/serviceWorkerAction');
 jest.mock('store/Provider/hooks');
 
 describe('useActiveLockStatus', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
   test('isPrompt is true, clear timer, and return successfully', () => {
-    jest.mocked(useActiveLockStatusAction).mockReturnValue(() => Promise.resolve());
+    jest.spyOn(serviceWorkerAction, 'useActiveLockStatusAction').mockReturnValue((): any => {
+      return Promise.resolve();
+    });
     jest.mocked(useCommonState).mockReturnValue({ isPrompt: true, isPopupInit: true, isNotLessThan768: false });
 
     const { result } = renderHook(() => useActiveLockStatus());
@@ -17,7 +24,9 @@ describe('useActiveLockStatus', () => {
     expect(result.current).toBeEnabled;
   });
   test('isPrompt is false, execute the activeLockStatus method, and return successfully', () => {
-    jest.mocked(useActiveLockStatusAction).mockReturnValue(() => Promise.resolve());
+    jest.spyOn(serviceWorkerAction, 'useActiveLockStatusAction').mockReturnValue((): any => {
+      return Promise.resolve();
+    });
     jest.mocked(useCommonState).mockReturnValue({ isPrompt: false, isPopupInit: true, isNotLessThan768: false });
 
     const { result } = renderHook(() => useActiveLockStatus());
@@ -27,11 +36,17 @@ describe('useActiveLockStatus', () => {
   });
 
   test('isPrompt is false, catch activeLockStatus error, and return successfully', () => {
-    jest.mocked(useActiveLockStatusAction).mockReturnValue(() => Promise.reject('error'));
+    jest.spyOn(serviceWorkerAction, 'useActiveLockStatusAction').mockReturnValue((): any => {
+      return Promise.reject({ code: 500 });
+    });
     jest.mocked(useCommonState).mockReturnValue({ isPrompt: false, isPopupInit: true, isNotLessThan768: false });
 
     const { result } = renderHook(() => useActiveLockStatus());
     result.current();
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
 
     expect(result.current).toBeEnabled;
   });

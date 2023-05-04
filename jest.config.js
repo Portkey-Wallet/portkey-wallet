03e1329f14fa81242e2918196651cb1004423ed4
sarah-portkey <sarah@portkey.finance>
2023-05-04 18:11:38 +0800

    test: 💍 jest config ignore

--- jest.config.js
+++ jest.config.js
@@ -64,7 +64,7 @@ module.exports = {
         '^constants/(.*)$': '<rootDir>/packages/web-extension-did/app/web/constants/$1',
         '^messages/(.*)$': '<rootDir>/packages/web-extension-did/app/web/messages/$1',
       },
-      coveragePathIgnorePatterns: ['/node_modules/', '/store/', '/hooks-ca/'],
+      coveragePathIgnorePatterns: ['/node_modules/', '/store/', '/hooks-ca/', '/utils/'],
     },
   ],
 };

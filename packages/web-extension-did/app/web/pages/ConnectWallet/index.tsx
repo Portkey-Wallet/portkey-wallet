bca45aea7b99119617473c87eedf13b3dff3d659
sarah-portkey <sarah@portkey.finance>
2023-06-06 19:00:33 +0800

    feat: 🎸 connect & transfer

--- packages/web-extension-did/app/web/pages/ConnectWallet/index.tsx
+++ packages/web-extension-did/app/web/pages/ConnectWallet/index.tsx
@@ -1,6 +1,85 @@
+import { addDapp } from '@portkey-wallet/store/store-ca/dapp/actions';
+import { Button } from 'antd';
+import CustomSvg from 'components/CustomSvg';
+import ImgLoading from 'components/ImgLoading';
 import usePromptSearch from 'hooks/usePromptSearch';
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useAppDispatch, useWalletInfo } from 'store/Provider/hooks';
+import errorHandler from 'utils/errorHandler';
+import { closePrompt } from 'utils/lib/serviceWorkerAction';
+import './index.less';
+
+const allowItem = [
+  'viewing wallet balance and activity Allow',
+  'sending requests for transactions',
+  'moving funds without your permission',
+];
 
 export default function ConnectWallet() {
   const detail = usePromptSearch();
-  return <div>{JSON.stringify(detail)}</div>;
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const { currentNetwork } = useWalletInfo();
+
+  const renderSite = useMemo(
+    () =>
+      detail && (
+        <div className="site flex-center">
+          <ImgLoading className="icon" src={detail.appLogo} loadEle={detail.appName[0]} errorEle={detail.appName[0]} />
+          <span>{detail.appHref}</span>
+        </div>
+      ),
+    [detail],
+  );
+
+  const renderAllow = useMemo(
+    () => (
+      <div className="allow">
+        {allowItem.map((item) => (
+          <div className="item" key={item}>
+            <div className="flex allow-title">
+              <CustomSvg type="TickFilled" className="flex-center" />
+              <span>{t('Allow')}</span>
+            </div>
+            <div className="allow-text">{item}</div>
+          </div>
+        ))}
+      </div>
+    ),
+    [t],
+  );
+
+  const handleSign = useCallback(() => {
+    dispatch(
+      addDapp({
+        networkType: currentNetwork,
+        dapp: detail,
+      }),
+    );
+    closePrompt({
+      ...errorHandler(0),
+      data: {},
+    });
+  }, [currentNetwork, detail, dispatch]);
+
+  return (
+    <div className="connect-wallet flex">
+      {renderSite}
+      <div className="title">{t('Connect with Portkey')}</div>
+      {renderAllow}
+      <div className="btn flex-between">
+        <Button
+          type="text"
+          onClick={() => {
+            closePrompt({ ...errorHandler(200003) });
+          }}>
+          {t('Reject')}
+        </Button>
+        <Button type="primary" onClick={handleSign}>
+          {t('Sign')}
+        </Button>
+      </div>
+    </div>
+  );
 }
--- packages/web-extension-did/app/web/pages/SendTransactions/index.tsx
+++ packages/web-extension-did/app/web/pages/SendTransactions/index.tsx
@@ -1,15 +1,22 @@
 import { useCurrentChain } from '@portkey-wallet/hooks/hooks-ca/chainList';
 import { useCurrentWalletInfo } from '@portkey-wallet/hooks/hooks-ca/wallet';
 import { ChainId } from '@portkey-wallet/types';
-import { handleErrorMessage } from '@portkey-wallet/utils';
+import { ZERO } from '@portkey-wallet/constants/misc';
+import { useIsMainnet } from '@portkey-wallet/hooks/hooks-ca/network';
+import { formatAmountShow } from '@portkey-wallet/utils/converter';
+import { formatChainInfoToShow, handleErrorMessage } from '@portkey-wallet/utils';
 import aes from '@portkey-wallet/utils/aes';
 import { Button, message } from 'antd';
+import CustomSvg from 'components/CustomSvg';
+import { useTranslation } from 'react-i18next';
 import usePromptSearch from 'hooks/usePromptSearch';
-import { useCallback } from 'react';
-import { useUserInfo } from 'store/Provider/hooks';
+import { useCallback, useEffect, useMemo } from 'react';
+import { useUserInfo, useWalletInfo } from 'store/Provider/hooks';
 import errorHandler from 'utils/errorHandler';
 import { closePrompt } from 'utils/lib/serviceWorkerAction';
 import { callSendMethod } from 'utils/sandboxUtil/sendTransactions';
+import { useAmountInUsdShow } from '@portkey-wallet/hooks/hooks-ca/useTokensPrice';
+import './index.less';
 
 export default function SendTransactions() {
   const detail = usePromptSearch<{
@@ -17,12 +24,78 @@ export default function SendTransactions() {
       chainId: ChainId;
       contractAddress: string;
       method: string;
+      isGetSignTx?: 0 | 1; // 0 message 1 detail
       params: any;
     };
   }>();
   const chainInfo = useCurrentChain(detail?.payload?.chainId);
   const wallet = useCurrentWalletInfo();
+  const { walletName } = useWalletInfo();
+  const { currentNetwork } = useWalletInfo();
+  const isMainnet = useIsMainnet();
+  const { t } = useTranslation();
+  const {
+    payload: { params },
+  } = detail;
   const { passwordSeed } = useUserInfo();
+  const amountInUsdShow = useAmountInUsdShow();
+
+  useEffect(() => {
+    // TODO fee
+  }, []);
+
+  const renderDetail = useMemo(() => {
+    const { symbol, amount, decimals } = params.paramsOption;
+    return (
+      <div className="detail">
+        <div className="title">Details</div>
+        <div className="amount">
+          <div className="title">Amount</div>
+          <div className="amount-number flex-between">
+            <div>{`${formatAmountShow(amount)} ELF`}</div>
+            {isMainnet && <div>{amountInUsdShow(amount, decimals, symbol)}</div>}
+          </div>
+        </div>
+        <div className="fee flex-between">
+          <div>Transaction Fee</div>
+          <div className="flex-center">
+            <div className="elf">{`${formatAmountShow(params.paramsOption.fee)} ELF`}</div>
+            {isMainnet && <div className="dollar">{amountInUsdShow(amount, decimals, symbol)}</div>}
+          </div>
+        </div>
+        <div className="total flex-between">
+          <div>Total (Amount + Transaction Fee)</div>
+          <div className="flex-center">
+            <div className="elf">{`${formatAmountShow(ZERO.plus(amount).plus(params.paramsOption.fee))} ELF`}</div>
+            {isMainnet && <div className="dollar">{amountInUsdShow(amount, decimals, symbol)}</div>}
+          </div>
+        </div>
+      </div>
+    );
+  }, [amountInUsdShow, isMainnet, params.paramsOption]);
+  const renderMessage = useMemo(() => {
+    const { messageDetail } = params.paramsOption;
+    return (
+      <div className="message-wrapper">
+        <div className="title">Message</div>
+        <div className="message">
+          <div className="title">String to be sign</div>
+          <div className="content">{messageDetail.sign}</div>
+          <div className="title">Method</div>
+          <div className="content">{messageDetail.method}</div>
+          <div className="title">Signature</div>
+          <div className="content">{messageDetail.signature}</div>
+        </div>
+        <div className="fee flex-between">
+          <div>Transaction Fee</div>
+          <div className="flex-center">
+            <div className="elf">{`${formatAmountShow(params.paramsOption.fee)} ELF`}</div>
+            {isMainnet && <div className="dollar">$0.12</div>}
+          </div>
+        </div>
+      </div>
+    );
+  }, [isMainnet, params.paramsOption]);
 
   const sendHandler = useCallback(async () => {
     try {
@@ -66,15 +139,37 @@ export default function SendTransactions() {
   }, [chainInfo, detail, wallet, passwordSeed]);
 
   return (
-    <div>
-      {JSON.stringify(detail)}
-      <Button
-        onClick={() => {
-          closePrompt(errorHandler(200003));
-        }}>
-        cancel
-      </Button>
-      <Button onClick={sendHandler}>Approve</Button>
+    <div className="send-transaction flex">
+      <div className="chain flex-center">
+        <CustomSvg type={isMainnet ? 'Aelf' : 'elf-icon'} />
+        <span>{formatChainInfoToShow(detail.payload.chainId, currentNetwork)}</span>
+      </div>
+      <div className="account flex">
+        <div className="name">{walletName}</div>
+        <CustomSvg type="Oval" />
+        <div className="address">{`${detail.payload.contractAddress.slice(
+          0,
+          10,
+        )}...${detail.payload.contractAddress.slice(-4)}`}</div>
+        <div className="line" />
+      </div>
+      <div className="method">
+        <div className="title">Method</div>
+        <div className="method-name">{detail.payload.method}</div>
+      </div>
+      {detail.payload.isGetSignTx ? renderMessage : renderDetail}
+      <div className="btn flex-between">
+        <Button
+          type="text"
+          onClick={() => {
+            closePrompt(errorHandler(200003));
+          }}>
+          {t('Reject')}
+        </Button>
+        <Button type="primary" onClick={sendHandler}>
+          {t('Sign')}
+        </Button>
+      </div>
     </div>
   );
 }

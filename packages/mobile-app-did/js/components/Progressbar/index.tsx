38b1d295dd5d013e4343114fa4132987244acfd1
Portkey-David <alex@portkey.finance>
2023-06-14 14:14:00 +0800

    refactor: 💡 progressBar

--- packages/mobile-app-did/js/components/Progressbar/index.tsx
+++ packages/mobile-app-did/js/components/Progressbar/index.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import { screenWidth } from '@portkey-wallet/utils/mobile/device';
+import { defaultColors } from 'assets/theme';
+import React, { forwardRef, useImperativeHandle, useMemo, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { pTd } from 'utils/unit';
-
-interface ProgressbarProps {
-  percentage: number;
+export interface IProgressbar {
+  changeInnerBarWidth: (per: number) => void;
 }
 
-export function Progressbar(props: ProgressbarProps) {
-  const { percentage } = props;
+const Progressbar = forwardRef<IProgressbar>(function Progressbar(props, ref) {
+  const [percentage, setPercentage] = useState<number>(0);
 
-  return <View style={styles.progressBar} />;
-}
+  const innerBarWidthStyle = useMemo(() => {
+    const num = Number(percentage);
+    return { width: `${num === 1 ? 0 : num * 100}%` };
+  }, [percentage]);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      changeInnerBarWidth(per: number) {
+        setPercentage(per);
+      },
+    }),
+    [],
+  );
+
+  return (
+    <View style={styles.progressBar}>
+      <View style={[styles.innerBar, innerBarWidthStyle]} />
+    </View>
+  );
+});
+
+export default Progressbar;
 
 const styles = StyleSheet.create({
   progressBar: {
-    width: '100%',
-    justifyContent: 'space-between',
-    paddingBottom: pTd(40),
+    zIndex: 100,
+    height: pTd(2),
+    width: screenWidth,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  innerBar: {
+    height: '100%',
+    zIndex: 101,
+    backgroundColor: defaultColors.bg8,
   },
 });

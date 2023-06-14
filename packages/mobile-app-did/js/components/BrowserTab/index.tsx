74e24e098a2009e6529a7c84bcf74ab7fd32f09c
portkey-yellow <yellow.m@portkey.finance>
2023-06-14 21:10:00 +0800

    fix: 🐛 types

--- packages/mobile-app-did/js/components/BrowserTab/index.tsx
+++ packages/mobile-app-did/js/components/BrowserTab/index.tsx
@@ -1,20 +1,20 @@
-import React, { forwardRef, memo, useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react';
+import React, { forwardRef, memo, useEffect, useImperativeHandle, useMemo, useRef } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { screenHeight, screenWidth } from '@portkey-wallet/utils/mobile/device';
 import ProviderWebview, { IWebView } from 'components/ProviderWebview';
 import { captureRef } from 'react-native-view-shot';
-import { useBrowser } from 'components/TabsDrawer/context';
-import Progressbar from 'components/Progressbar';
+import { IBrowserTab, useBrowser } from 'components/TabsDrawer/context';
+import Progressbar, { IProgressbar } from 'components/Progressbar';
 
 type BrowserTabProps = {
   isHidden: boolean;
   uri: string;
 };
 
-const BrowserTab = forwardRef<any, BrowserTabProps>(function BrowserTab({ isHidden, uri }, forward) {
+const BrowserTab = forwardRef<IBrowserTab, BrowserTabProps>(function BrowserTab({ isHidden, uri }, forward) {
   const viewRef = useRef<any>(null);
   const webViewRef = useRef<IWebView | null>(null);
-  const progressbarRef = useRef<any>(null);
+  const progressbarRef = useRef<IProgressbar>(null);
 
   const { setTabRef } = useBrowser();
 
@@ -42,7 +42,7 @@ const BrowserTab = forwardRef<any, BrowserTabProps>(function BrowserTab({ isHidd
       <ProviderWebview
         ref={webViewRef}
         source={{ uri }}
-        onLoadProgress={({ nativeEvent }) => progressbarRef.current.changeInnerBarWidth(nativeEvent.progress)}
+        onLoadProgress={({ nativeEvent }) => progressbarRef.current?.changeInnerBarWidth(nativeEvent.progress)}
       />
     </View>
   );

ba7ee17c308ee99a9bc2f1b5d2118617bd8d8469
thomas-portkey <thomas@portkey.finance>
2023-06-15 10:39:37 +0800

    feat: 🎸 add checkGoogleRecaptcha params

--- packages/web-extension-did/app/web/utils/lib/checkReCaptcha.ts
+++ packages/web-extension-did/app/web/utils/lib/checkReCaptcha.ts
@@ -1,3 +1,4 @@
+import { RecaptchaType } from '@portkey-wallet/types/verifier';
 import { reCAPTCHAAction } from './serviceWorkerAction';
 import { request } from '@portkey-wallet/api/api-did';
 
@@ -5,9 +6,13 @@ import { request } from '@portkey-wallet/api/api-did';
  * check is need to call Google reCAPTCHA
  * @returns {string} check response
  */
-export const checkReCaptcha = async (isNeedRecaptcha = false) => {
+export const checkReCaptcha = async (operationType = RecaptchaType.register, isNeedRecaptcha = false) => {
   if (!isNeedRecaptcha) {
-    const req = await request.verify.checkGoogleRecaptcha();
+    const req = await request.verify.checkGoogleRecaptcha({
+      params: {
+        operationType,
+      },
+    });
     isNeedRecaptcha = !!req;
   }
 

bfa5213574cbf857e8b6f75da37800624429e0a5
Portkey-David <alex@portkey.finance>
2023-07-05 11:04:37 +0800

    feat: 🎸 text

--- packages/mobile-app-did/js/hooks/login.ts
+++ packages/mobile-app-did/js/hooks/login.ts
@@ -105,7 +105,7 @@ export function useOnManagerAddressAndQueryResult() {
       verifierInfo?: VerifierInfo;
       guardiansApproved?: GuardiansApproved;
     }) => {
-      showLoading && Loading.show({ text: t('Working on recovery...') });
+      showLoading && Loading.show({ text: t('Initiating social recovery...') });
       await sleep(500);
       const isRecovery = managerInfo.verificationType === VerificationType.communityRecovery;
       try {

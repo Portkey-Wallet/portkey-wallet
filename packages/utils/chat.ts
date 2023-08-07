ad0a46b78c3dbf4ff1675307604c369c592e2bce
Portkey-David <alex@portkey.finance>
2023-08-07 19:53:17 +0800

    feat: 🎸 add format chatList tim

--- packages/utils/chat.ts
+++ packages/utils/chat.ts
@@ -1,4 +1,24 @@
+import dayjs from 'dayjs';
 export const formatMessageNumToStr = (num: number): string | undefined => {
   if (!num || num < 0) return undefined;
   return num > 99 ? '99+' : String(num);
+};
+
+export const formatChatListTime = (timeStamp: number): string => {
+  const chatTime = dayjs(timeStamp);
+
+  const now = dayjs();
+  const today = now.startOf('date');
+  const yesterday = today.subtract(1, 'day');
+  const thisYear = now.startOf('year');
+
+  if (chatTime.isAfter(today)) {
+    return chatTime.format('HH:mm');
+  } else if (chatTime.isAfter(yesterday)) {
+    return 'yesterday';
+  } else if (chatTime.isAfter(thisYear)) {
+    return chatTime.format('MM-DD');
+  } else {
+    return chatTime.format('YYYY-MM-DD');
+  }
 };

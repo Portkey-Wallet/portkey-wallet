40ca641dee4ce9870d25bdd371ee1c691f2ded83
Portkey-David <alex@portkey.finance>
2023-08-16 18:08:42 +0800

    feat: 🎸 my wallet

--- packages/mobile-app-did/js/pages/My/WalletHome/WalletName/index.tsx
+++ packages/mobile-app-did/js/pages/My/WalletHome/WalletName/index.tsx
@@ -1,7 +1,6 @@
 import PageContainer from 'components/PageContainer';
 import { useLanguage } from 'i18n/hooks';
-import React, { useCallback, useState } from 'react';
-import { pageStyles } from './style';
+import React, { useCallback, useMemo, useState } from 'react';
 import CommonButton from 'components/CommonButton';
 import CommonInput from 'components/CommonInput';
 import { ErrorType } from 'types/common';
@@ -9,18 +8,25 @@ import { INIT_HAS_ERROR } from 'constants/common';
 import { isValidCAWalletName } from '@portkey-wallet/utils/reg';
 import navigationService from 'utils/navigationService';
 import CommonToast from 'components/CommonToast';
-import { useSetWalletName, useWallet } from '@portkey-wallet/hooks/hooks-ca/wallet';
+import { useCurrentCaInfo, useSetWalletName, useWallet } from '@portkey-wallet/hooks/hooks-ca/wallet';
 import Loading from 'components/Loading';
 import FormItem from 'components/FormItem';
-import { View } from 'react-native';
-import { TextM } from 'components/CommonText';
-import Svg from 'components/Svg';
-import { pTd } from 'utils/unit';
-import Touchable from 'components/Touchable';
+import { ScrollView, StyleSheet } from 'react-native';
+import ProfilePortkeyIDSection from 'pages/My/components/ProfilePortkeyIDSection';
+import ProfileHeaderSection from 'pages/My/components/ProfileHeaderSection';
+import ProfileDIDSection from 'pages/My/components/ProfileDIDSection';
+
+import { defaultColors } from 'assets/theme';
+import GStyles from 'assets/theme/GStyles';
+import { CAInfo } from '@portkey-wallet/types/types-ca/wallet';
+import { ChainId } from '@portkey-wallet/types';
 
 const WalletName: React.FC = () => {
   const { t } = useLanguage();
-  const { walletName } = useWallet();
+  const { walletName, userId } = useWallet();
+  const caInfo = useCurrentCaInfo();
+
+  const [editing, setEditing] = useState(false);
   const [nameValue, setNameValue] = useState<string>(walletName);
   const [nameError, setNameError] = useState<ErrorType>(INIT_HAS_ERROR);
   const setWalletName = useSetWalletName();
@@ -29,6 +35,20 @@ const WalletName: React.FC = () => {
     setNameValue(value);
     setNameError({ ...INIT_HAS_ERROR });
   }, []);
+
+  const caInfoList = useMemo(() => {
+    const result: { address: string; chainId: ChainId }[] = [];
+    Object.entries(caInfo || {}).map(([key, value]) => {
+      const info = value as CAInfo;
+      if (info?.caAddress) {
+        result.push({
+          address: info?.caAddress,
+          chainId: key as ChainId,
+        });
+      }
+    });
+    return result;
+  }, [caInfo]);
 
   const onSave = useCallback(async () => {
     const _nameValue = nameValue.trim();
@@ -61,47 +81,51 @@ const WalletName: React.FC = () => {
     Loading.hide();
   }, [nameValue, setWalletName, t]);
 
+  const onEdit = useCallback(() => setEditing(true), []);
+
   return (
     <PageContainer
-      titleDom={t('Wallet Name')}
+      titleDom={t(editing ? 'Edit' : 'My DID')}
       safeAreaColor={['blue', 'gray']}
       containerStyles={pageStyles.pageWrap}
       scrollViewProps={{ disabled: true }}>
-      <FormItem title={'Wallet Name'}>
-        <CommonInput
-          type="general"
-          spellCheck={false}
-          autoCorrect={false}
-          value={nameValue}
-          theme={'white-bg'}
-          placeholder={t('Enter Wallet Name')}
-          onChangeText={onNameChange}
-          maxLength={16}
-          errorMessage={nameError.errorMsg}
-        />
-      </FormItem>
+      <ScrollView>
+        {editing ? (
+          <FormItem title={'Wallet Name'}>
+            <CommonInput
+              type="general"
+              spellCheck={false}
+              autoCorrect={false}
+              value={nameValue}
+              theme={'white-bg'}
+              placeholder={t('Enter Wallet Name')}
+              onChangeText={onNameChange}
+              maxLength={16}
+              errorMessage={nameError.errorMsg}
+            />
+          </FormItem>
+        ) : (
+          <ProfileHeaderSection name={nameValue} />
+        )}
+        <ProfilePortkeyIDSection disable={editing} portkeyID={userId || ''} />
+        <ProfileDIDSection disable={editing} addressList={caInfoList} />
+      </ScrollView>
 
-      <FormItem title={'Portkey ID'}>
-        <View>
-          <TextM>xxxxxx-yyyy-zzzzz</TextM>
-          <Touchable>
-            <Svg icon="copy" size={pTd(20)} />
-          </Touchable>
-        </View>
-      </FormItem>
-      <FormItem title={'DID'}>
-        <View>
-          <Svg icon="elf-icon" />
-          <TextM>xxxxxx-yyyy-zzzzz</TextM>
-          <Touchable>
-            <Svg icon="copy" size={pTd(20)} />
-          </Touchable>
-        </View>
-      </FormItem>
-      <CommonButton disabled={nameValue === ''} type="solid" onPress={onSave}>
-        {t('Save')}
-      </CommonButton>
+      <CommonButton
+        title={t(editing ? 'Save' : 'Edit')}
+        disabled={nameValue === ''}
+        type="solid"
+        onPress={editing ? onSave : onEdit}
+      />
     </PageContainer>
   );
 };
 export default WalletName;
+
+export const pageStyles = StyleSheet.create({
+  pageWrap: {
+    flex: 1,
+    backgroundColor: defaultColors.bg4,
+    ...GStyles.paddingArg(24, 20, 18),
+  },
+});
---
+++ packages/mobile-app-did/js/pages/My/components/ProfileDIDSection/index.tsx
@@ -0,0 +1,75 @@
+import { useWallet } from '@portkey-wallet/hooks/hooks-ca/wallet';
+import { ChainId, ChainType } from '@portkey-wallet/types';
+import { addressFormat, formatChainInfoToShow, formatStr2EllipsisStr } from '@portkey-wallet/utils';
+import { defaultColors } from 'assets/theme';
+import GStyles from 'assets/theme/GStyles';
+import { BGStyles, FontStyles } from 'assets/theme/styles';
+import { TextM, TextS } from 'components/CommonText';
+import FormItem from 'components/FormItem';
+import Svg from 'components/Svg';
+import Touchable from 'components/Touchable';
+
+import React, { memo, useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { copyText } from 'utils';
+import { pTd } from 'utils/unit';
+
+type addressItemType = {
+  address: string;
+  chainId: ChainId;
+  chainType?: ChainType;
+};
+
+type PortkeyIDItemPropsType = {
+  disable?: boolean;
+  addressList: addressItemType[];
+};
+
+const PortkeyIDItem: React.FC<PortkeyIDItemPropsType> = props => {
+  const { disable, addressList } = props;
+  const { currentNetwork } = useWallet();
+
+  const copyId = useCallback(
+    (ele: addressItemType) =>
+      copyText(ele.chainType === 'ethereum' ? ele.address : `ELF_${ele.address}_${ele.chainId}`),
+    [],
+  );
+
+  return (
+    <FormItem title="DID" style={GStyles.marginTop(pTd(24))}>
+      {addressList.map((ele, index) => (
+        <View key={index} style={[disable ? BGStyles.bg18 : BGStyles.bg1, styles.itemWrap]}>
+          <View style={[GStyles.flexRow, GStyles.itemCenter, GStyles.spaceBetween, styles.content]}>
+            <TextM style={styles.address}>{formatStr2EllipsisStr(addressFormat(ele.address, ele.chainId), 20)}</TextM>
+            <Touchable onPress={() => copyId(ele)}>
+              <Svg icon="copy" size={pTd(16)} />
+            </Touchable>
+          </View>
+          <View style={GStyles.flexRow}>
+            <Svg icon="elf-icon" size={pTd(16)} />
+            <TextS style={[FontStyles.font3, GStyles.marginLeft(pTd(8))]}>
+              {formatChainInfoToShow(ele.chainId, currentNetwork)}
+            </TextS>
+          </View>
+        </View>
+      ))}
+    </FormItem>
+  );
+};
+
+export default memo(PortkeyIDItem);
+
+const styles = StyleSheet.create({
+  itemWrap: {
+    padding: pTd(16),
+    borderRadius: pTd(6),
+    marginBottom: pTd(8),
+  },
+  content: {
+    marginBottom: pTd(8),
+  },
+  address: {
+    width: pTd(270),
+    color: defaultColors.font5,
+  },
+});

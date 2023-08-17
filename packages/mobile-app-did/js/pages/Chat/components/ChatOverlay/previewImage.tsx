96b450d18f779ad7f30a9e70e9ce9d3695955a86
portkey-yellow <yellow.m@portkey.finance>
2023-08-17 10:20:04 +0800

    feat: 🎸 preview image

--- packages/mobile-app-did/js/pages/Chat/components/ChatOverlay/previewImage.tsx
+++ packages/mobile-app-did/js/pages/Chat/components/ChatOverlay/previewImage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { screenWidth, windowHeight, windowWidth } from '@portkey-wallet/utils/mobile/device';
 import OverlayModal, { CustomBounds } from 'components/OverlayModal';
 import { ImageProps, StyleSheet } from 'react-native';
@@ -8,11 +8,16 @@ import TransformView from 'rn-teaset/components/TransformView/TransformView';
 type PreviewImageProps = { source: ImageProps['source']; thumb?: ImageProps['source'] };
 
 function PreviewImage({ source, thumb }: PreviewImageProps) {
+  const scaleRef = useRef<number>(1);
   return (
     <TransformView
       onPress={OverlayModal.hide}
       onDidTransform={(_: number, translateY: number) => {
+        if (scaleRef.current !== 1) return;
         if (Math.abs(translateY) > 50) OverlayModal.hide();
+      }}
+      onTransforming={(translateX: number, translateY: number, scale: number) => {
+        scaleRef.current = scale;
       }}
       style={GStyles.flex1}
       containerStyle={styles.containerStyle}>
@@ -36,5 +41,5 @@ export function showPreviewImage({
 }
 
 const styles = StyleSheet.create({
-  containerStyle: { height: windowHeight * 0.8, width: windowWidth * 0.8 },
+  containerStyle: { height: windowHeight * 0.8, width: windowWidth },
 });

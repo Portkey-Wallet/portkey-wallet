53cbd147b67adde7846e818002cfac495e38691e
kong lq <qin@portkey.finance>
2023-08-18 11:13:11 +0800

    feat: add firebase user property

--- packages/mobile-app-did/js/hooks/useInitData.ts
+++ packages/mobile-app-did/js/hooks/useInitData.ts
@@ -3,18 +3,21 @@ import { useCurrentWalletInfo } from '@portkey-wallet/hooks/hooks-ca/wallet';
 import { fetchBuyFiatListAsync, fetchSellFiatListAsync } from '@portkey-wallet/store/store-ca/payment/actions';
 import { getSymbolImagesAsync } from '@portkey-wallet/store/store-ca/tokenManagement/action';
 import { getWalletNameAsync } from '@portkey-wallet/store/store-ca/wallet/actions';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useAppDispatch } from 'store/hooks';
 import { useGetCurrentCAViewContract } from './contract';
 import { useGetGuardiansInfoWriteStore, useGetVerifierServers } from './guardian';
 import useEffectOnce from './useEffectOnce';
 import { useBookmarkList, useCheckAndInitNetworkDiscoverMap } from './discover';
+import { useCurrentNetwork } from '@portkey-wallet/hooks/network';
+import { reportUserCurrentNetwork } from 'utils/analysisiReport';
 
 export default function useInitData() {
   const dispatch = useAppDispatch();
   const getCurrentCAViewContract = useGetCurrentCAViewContract();
   const { caHash } = useCurrentWalletInfo();
   const getVerifierServers = useGetVerifierServers();
+  const { netWorkType } = useCurrentNetwork();
 
   const getGuardiansInfoWriteStore = useGetGuardiansInfoWriteStore();
   const isMainNetwork = useIsMainnet();
@@ -56,4 +59,9 @@ export default function useInitData() {
     const timer = setTimeout(init, 500);
     return () => clearTimeout(timer);
   });
+
+  useEffect(() => {
+    // report user`s current environment of network
+    reportUserCurrentNetwork(netWorkType);
+  }, [netWorkType]);
 }
---
+++ packages/utils/analyticsReport.ts
@@ -0,0 +1,19 @@
+import { request } from '@portkey-wallet/api/api-did';
+
+export const enum CUSTOM_EVENT_ENUM {
+  LOGIN = 'login_custom',
+}
+
+export const enum NETWORK_TYPE {
+  MAIN = 'main',
+  TESTNET = 'testnet',
+}
+
+export type eventParamsType = {
+  network_type?: NETWORK_TYPE;
+};
+
+export const checkEnvironmentIsProduction = () => {
+  const url = request.defaultConfig.baseURL || '';
+  return /(http(s?):\/\/)?did-portkey.*(\.portkey\.finance)$/.test(url);
+};

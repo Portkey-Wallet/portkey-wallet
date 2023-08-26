30c074ca7545131ffbf4c9bae28126cfb4bb66a0
portkey-yellow <yellow.m@portkey.finance>
2023-08-26 15:21:06 +0800

    fix: 🐛 max input length

--- packages/constants/constants-ca/im.ts
+++ packages/constants/constants-ca/im.ts
@@ -5,5 +5,5 @@ export enum IMErrorEnum {
 export const CHANNEL_LIST_LIMIT = 20;
 export const MESSAGE_LIST_LIMIT = 50;
 export const SEARCH_CHANNEL_LIMIT = 50;
-export const MAX_INPUT_LENGTH = 300;
+export const MAX_INPUT_LENGTH = 500;
 export const MAX_FILE_SIZE = 10; // 10M

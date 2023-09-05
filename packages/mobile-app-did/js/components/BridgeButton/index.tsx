551166bcf2956f16b59fde5bd2f41cb27dbd952e
Portkey-David <alex@portkey.finance>
2023-09-05 20:41:26 +0800

    feat: 🎸 button style

--- packages/mobile-app-did/js/components/BridgeButton/index.tsx
+++ packages/mobile-app-did/js/components/BridgeButton/index.tsx
@@ -1,49 +1,30 @@
 import React, { memo } from 'react';
 import Svg from 'components/Svg';
-import navigationService from 'utils/navigationService';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, StyleProp, ViewProps } from 'react-native';
 import { TextM } from 'components/CommonText';
 import { useLanguage } from 'i18n/hooks';
 import { pTd } from 'utils/unit';
 import GStyles from 'assets/theme/GStyles';
-import { defaultColors } from 'assets/theme';
+import { commonButtonStyle } from '../SendButton/style';
 
-const BuyButton = () => {
+type BridgeButtonPropsType = {
+  wrapStyle?: StyleProp<ViewProps>;
+};
+
+const BridgeButton = (props: BridgeButtonPropsType) => {
+  const { wrapStyle = {} } = props;
   const { t } = useLanguage();
 
   return (
-    <View style={styles.buttonWrap}>
-      <TouchableOpacity
-        style={[styles.iconWrapStyle, GStyles.alignCenter]}
-        onPress={async () => {
-          navigationService.navigate('BuyHome');
-        }}>
-        <Svg icon="Contract" size={pTd(46)} />
+    <View style={[commonButtonStyle.buttonWrap, wrapStyle]}>
+      <TouchableOpacity style={[commonButtonStyle.iconWrapStyle, GStyles.alignCenter]}>
+        <Svg icon="send" size={pTd(46)} />
       </TouchableOpacity>
-      <TextM style={styles.titleStyle}>{t('Bridge')}</TextM>
+      <TextM style={[commonButtonStyle.commonTitleStyle, commonButtonStyle.dashBoardTitleColorStyle]}>
+        {t('Bridge')}
+      </TextM>
     </View>
   );
 };
 
-export default memo(BuyButton);
-
-export const styles = StyleSheet.create({
-  buttonWrap: {
-    marginBottom: pTd(24),
-    width: pTd(54),
-  },
-  iconWrapStyle: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-  titleStyle: {
-    width: '100%',
-    marginTop: pTd(4),
-    textAlign: 'center',
-    color: defaultColors.font2,
-    fontSize: pTd(14),
-    lineHeight: pTd(20),
-  },
-});
+export default memo(BridgeButton);

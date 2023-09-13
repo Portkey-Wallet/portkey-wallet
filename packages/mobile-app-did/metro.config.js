4266e2c26951626379eeeeb7f27c6c0ce45f0685
portkey-yellow <yellow.m@portkey.finance>
2023-09-13 17:44:06 +0800

    feat: 🎸 update react native

--- packages/mobile-app-did/metro.config.js
+++ packages/mobile-app-did/metro.config.js
@@ -2,12 +2,19 @@
 // Learn more https://docs.expo.dev/guides/monorepos
 // Learn more https://facebook.github.io/metro/docs/configuration/
 const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
-module.exports = {
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   // Watch all files within the monorepo
   watchFolders: [workspaceRoot],
   resolver: {
@@ -16,12 +23,5 @@ module.exports = {
     // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
     disableHierarchicalLookup: true,
   },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
 };
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);

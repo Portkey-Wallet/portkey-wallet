7f97e5ad99046aba5c3ae6dbb02b641edcec292e
portkey-yellow <yellow.m@portkey.finance>
2023-09-20 19:55:27 +0800

    feat: 🎸 link path

--- packages/constants/constants-ca/network-test2.ts
+++ packages/constants/constants-ca/network-test2.ts
@@ -1,4 +1,5 @@
 import { NetworkItem } from '@portkey-wallet/types/types-ca/network';
+import { LINK_PATH_ENUM } from './link';
 
 export const NetworkList: NetworkItem[] = [
   {
@@ -26,6 +27,9 @@ export const OfficialWebsite = 'https://portkey.finance';
 
 export const ThirdParty = `https://openlogin-test.portkey.finance`;
 
-export const LinkWebsite = 'https://portkey-website-dev.vercel.app';
+export const LinkPortkeyWebsite = 'https://portkey-website-dev.vercel.app';
 
-export const AddContactLinkPath = LinkWebsite + '/sc/ac/';
+export enum LinkPortkeyPath {
+  addContact = LinkPortkeyWebsite + LINK_PATH_ENUM.addContact,
+  addGroup = LinkPortkeyWebsite + LINK_PATH_ENUM.addGroup,
+}

13fb729ccd427de3bddd3d36909bc6501e1b3804
wade-portkey <codingwizard@portkey.finance>
2023-09-26 10:47:24 +0800

    feat: ios remove pods directory

--- packages/rn-sdk/ios/Example/Pods/Target Support Files/Pods-PortkeySDKExample_Example/Pods-PortkeySDKExample_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PortkeySDKExample_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PortkeySDKExample_Example
-@end
--- packages/rn-sdk/ios/Example/Pods/Target Support Files/PortkeySDK/PortkeySDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PortkeySDK : NSObject
-@end
-@implementation PodsDummy_PortkeySDK
-@end

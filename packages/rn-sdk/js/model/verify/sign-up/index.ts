cd4fbe9480085ade7b96ae5482c5b95f1637f08a
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-10-09 17:33:36 +0800

    feat: decouple send-verify-code and guardian-verify

--- packages/rn-sdk/js/model/verify/sign-up/index.ts
+++ packages/rn-sdk/js/model/verify/sign-up/index.ts
@@ -2,6 +2,8 @@ import { useCallback, useState } from 'react';
 import { GuardianConfig } from '../guardian';
 import { AccountOriginalType, AfterVerifiedConfig, VerifiedGuardianDoc } from '../after-verify';
 import { DeviceType } from '@portkey-wallet/types/types-ca/device';
+import { NetworkController } from 'network/controller';
+import { OperationTypeEnum } from '@portkey-wallet/types/verifier';
 
 const useSignUp = (config: SignUpConfig) => {
   const [verifiedGuardianInfo, setVerifiedGuardianInfo] = useState<VerifiedGuardianDoc | null>(null);
@@ -18,11 +20,42 @@ const useSignUp = (config: SignUpConfig) => {
     } as Partial<AfterVerifiedConfig>;
   }, [config, isVerified, verifiedGuardianInfo]);
 
+  const sendVerifyCode = useCallback(
+    async (googleRecaptchaToken?: string): Promise<boolean> => {
+      const needGoogleRecaptcha = await NetworkController.isGoogleRecaptchaOpen(
+        config.guardianConfig.sendVerifyCodeParams.operationType,
+      );
+      if (needGoogleRecaptcha && !googleRecaptchaToken) {
+        console.warn('Need google recaptcha! Better check it before calling this function.');
+        return false;
+      }
+      const result = await NetworkController.sendVerifyCode(
+        config.guardianConfig.sendVerifyCodeParams,
+        googleRecaptchaToken ? { reCaptchaToken: googleRecaptchaToken } : {},
+      );
+      if (result.verifierSessionId) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    [config],
+  );
+
+  const isGoogleRecaptchaOpen = async () => {
+    return await NetworkController.isGoogleRecaptchaOpen(OperationTypeEnum.register);
+  };
+
+  const goToGuardianVerifyPage = useCallback(() => {
+    config.navigateToGuardianPage(setVerifiedGuardianInfo);
+  }, [config]);
+
   return {
-    status,
     isVerified,
-    setVerifiedGuardianInfo,
     getVerifiedData,
+    isGoogleRecaptchaOpen,
+    sendVerifyCode,
+    goToGuardianVerifyPage,
   };
 };
 
@@ -30,6 +63,9 @@ export interface SignUpConfig {
   accountIdentifier: string;
   accountOriginalType: AccountOriginalType;
   guardianConfig: GuardianConfig;
+  isGoogleRecaptchaOpen: () => Promise<boolean>;
+  sendVerifyCode: (googleRecaptchaToken?: string) => Promise<boolean>;
+  navigateToGuardianPage: (callback: (data: VerifiedGuardianDoc) => void) => void;
 }
 
 export default useSignUp;

0394434debede5ef95c40b98e57b46c687e182e0
Portkey-David <alex@portkey.finance>
2023-10-09 16:34:20 +0800

    feat: 🎸 security safe check

--- packages/mobile-app-did/js/components/BridgeButton/index.tsx
+++ packages/mobile-app-did/js/components/BridgeButton/index.tsx
@@ -6,40 +6,34 @@ import { useLanguage } from 'i18n/hooks';
 import { pTd } from 'utils/unit';
 import GStyles from 'assets/theme/GStyles';
 import { commonButtonStyle } from '../SendButton/style';
-import { useCurrentWalletInfo } from '@portkey-wallet/hooks/hooks-ca/wallet';
-import WalletSecurityOverlay from 'components/WalletSecurityOverlay';
-import { checkSecurity } from '@portkey-wallet/utils/securityTest';
 import Loading from 'components/Loading';
 import CommonToast from 'components/CommonToast';
 import DisclaimerModal from 'components/DisclaimerModal';
 import { useDisclaimer } from '@portkey-wallet/hooks/hooks-ca/disclaimer';
 import { useCurrentNetworkInfo } from '@portkey-wallet/hooks/hooks-ca/network';
 import navigationService from 'utils/navigationService';
+import { useSecuritySafeCheckAndToast } from 'hooks/security';
 
 type BridgeButtonPropsType = {
   wrapStyle?: StyleProp<ViewProps>;
 };
 
 const BridgeButton = (props: BridgeButtonPropsType) => {
-  const { wrapStyle = {} } = props;
   const { t } = useLanguage();
-  const { caHash } = useCurrentWalletInfo();
+  const { wrapStyle = {} } = props;
   const { eBridgeUrl } = useCurrentNetworkInfo();
   const { checkDappIsConfirmed } = useDisclaimer();
+  const securitySafeCheckAndToast = useSecuritySafeCheckAndToast();
 
   return (
     <View style={[commonButtonStyle.buttonWrap, wrapStyle]}>
       <TouchableOpacity
         style={[commonButtonStyle.iconWrapStyle, GStyles.alignCenter]}
         onPress={async () => {
-          if (!caHash) return;
-
           try {
             Loading.show();
-            const isSafe = await checkSecurity(caHash);
-            if (!isSafe) return WalletSecurityOverlay.alert();
+            if (!(await securitySafeCheckAndToast())) return;
             if (!checkDappIsConfirmed(eBridgeUrl || '')) return DisclaimerModal.showConnectModal();
-
             navigationService.navigate('EBridge');
           } catch (error) {
             CommonToast.failError(error);

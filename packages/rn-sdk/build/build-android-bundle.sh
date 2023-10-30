a688705850418cc516e3ef9f749c0a88eb3bc61d
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-10-30 14:16:49 +0800

    feat: auto pack-up for Android

--- packages/rn-sdk/build/build-android-bundle.sh
+++ packages/rn-sdk/build/build-android-bundle.sh
@@ -1,2 +1,3 @@
 # build index.android.bundle
+cd ../../ # this is used on /packages/rn-sdk/android/library, if you need to run this script, just run the next line on /packages/rn-sdk
 react-native bundle --platform android --entry-file index.js --bundle-output android/library/src/main/assets/index.android.bundle --assets-dest android/library/src/main/res/  --dev true

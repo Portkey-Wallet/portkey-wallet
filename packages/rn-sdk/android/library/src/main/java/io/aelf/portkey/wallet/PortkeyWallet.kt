e2cf4b172a8587016a2d91098df6b99d37949382
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-11-03 15:07:44 +0800

    feat: use tempStorage in Android

--- packages/rn-sdk/android/library/src/main/java/io/aelf/portkey/wallet/PortkeyWallet.kt
+++ packages/rn-sdk/android/library/src/main/java/io/aelf/portkey/wallet/PortkeyWallet.kt
@@ -1,5 +1,14 @@
 package io.aelf.portkey.wallet
 
+import io.aelf.portkey.components.logic.PortkeyMMKVStorage
+import io.aelf.portkey.components.logic.TEMP
+import io.aelf.portkey.components.logic.but
+
 class PortkeyWallet {
 
+}
+
+internal fun isWalletUnlocked(): Boolean {
+    val walletConfig = PortkeyMMKVStorage.readString("walletConfig" but TEMP)
+    return walletConfig?.isNotEmpty() ?: false
 }

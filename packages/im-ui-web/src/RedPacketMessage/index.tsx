1905c61c2a3279413f2941640e121d89d2080db4
sarah-portkey <sarah@portkey.finance>
2023-11-22 14:03:18 +0800

    feat: 🎸 red packet can not be delete

--- packages/im-ui-web/src/RedPacketMessage/index.tsx
+++ packages/im-ui-web/src/RedPacketMessage/index.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useMemo, useState, useCallback } from 'react';
-import { Popover } from 'antd';
+import React, { useMemo } from 'react';
 import clsx from 'clsx';
 import { IRedPacketMessageProps } from '../type';
 import { formatTime } from '../utils';
-import PopoverMenuList from '../PopoverMenuList';
-import CustomSvg from '../components/CustomSvg';
 import { RedPacketTextByMine, RedPacketTextByOthers } from '../constants';
 import './index.less';
 
@@ -13,55 +10,19 @@ const RedPacketMessage: React.FC<IRedPacketMessageProps> = (props) => {
     () => (props.dateString ? props.dateString : formatTime(props.date as any)),
     [props.date, props.dateString],
   );
-  const [popVisible, setPopVisible] = useState(false);
-  const hidePop = useCallback(() => {
-    setPopVisible(false);
-  }, []);
-  const popoverList = useMemo(
-    () => [
-      {
-        key: 'delete',
-        leftIcon: <CustomSvg type="Delete" />,
-        children: 'Delete',
-        onClick: (e: React.MouseEvent<HTMLElement>) => props?.onDeleteMsg?.(e),
-      },
-    ],
-    [props],
-  );
   const renderContainer = useMemo(() => {
     const redPacketText = props.position === 'right' ? RedPacketTextByMine : RedPacketTextByOthers;
     return (
       <div className={clsx(['red-packet-body', 'flex', props.position])}>
         <div className="text-red-packet">
           {redPacketText}
-          <span className="text-red-packet-hidden">{showDate}</span>
+          <span className="red-packet-date-hidden">{showDate}</span>
         </div>
         <div className="red-packet-date">{showDate}</div>
       </div>
     );
   }, [props.position, showDate]);
-  useEffect(() => {
-    document.addEventListener('click', hidePop);
-    return () => document.removeEventListener('click', hidePop);
-  }, [hidePop]);
-  return (
-    <div className="portkey-message-red-packet">
-      {props.position === 'right' ? (
-        <Popover
-          open={popVisible}
-          overlayClassName={clsx(['message-red-packet-popover', props.position])}
-          placement="bottom"
-          trigger="contextMenu"
-          onOpenChange={(visible) => setPopVisible(visible)}
-          showArrow={false}
-          content={<PopoverMenuList data={popoverList} />}>
-          {renderContainer}
-        </Popover>
-      ) : (
-        renderContainer
-      )}
-    </div>
-  );
+  return <div className="portkey-message-red-packet">{renderContainer}</div>;
 };
 
 export default RedPacketMessage;

e04d56154c665dfd46047e8fd2fde6521c67ff15
portkey-yellow <yellow.m@portkey.finance>
2023-12-02 14:20:56 +0800

    feat: 🎸 fcm img

---
+++ packages/mobile-app-did/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by sky on 2023/12/2.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

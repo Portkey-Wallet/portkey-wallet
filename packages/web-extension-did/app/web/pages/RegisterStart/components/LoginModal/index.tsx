402823c724003f17b060eeb89d019a2274dbf2c4
sarah-portkey <sarah@portkey.finance>
2024-01-05 20:04:46 +0800

    style: 💄 international doc

--- packages/web-extension-did/app/web/pages/RegisterStart/components/LoginModal/index.tsx
+++ packages/web-extension-did/app/web/pages/RegisterStart/components/LoginModal/index.tsx
@@ -22,7 +22,7 @@ export default function LoginModal({
     }
     if (type === 'login') {
       if (serviceSuspension?.isSuspended) {
-        return 'This account has not been registered yet. If you want to register with this account, please download Portkey V2 to get better experience.';
+        return `This account is not registered yet. If you wish to create a Portkey account, we recommend using the fully upgraded Portkey for an enhanced experience.`;
       }
       return 'This account has not been registered yet. Click "Confirm" to complete the registration.';
     }

386e5aea8350118de3919fcb00affa342f6f4798
ykx <yangkexin@portkey.finance>
2024-01-17 18:57:15 +0800

    fix: 🐛 contact detail - clear api data

--- packages/web-extension-did/app/web/pages/Contacts/ViewContact/index.tsx
+++ packages/web-extension-did/app/web/pages/Contacts/ViewContact/index.tsx
@@ -104,6 +104,9 @@ export default function ViewContact() {
   useEffect(() => {
     if (!showChat) return;
 
+    // clear api data
+    setProfileData({});
+
     im.service
       .getProfile({
         id: state.id || undefined,

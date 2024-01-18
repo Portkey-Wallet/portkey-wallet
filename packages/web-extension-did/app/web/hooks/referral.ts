8412053376068b4ccda4cf896ebfaf12d8c5090e
sarah-portkey <sarah@portkey.finance>
2024-01-18 17:13:10 +0800

    feat: 🎸 add referral url

--- packages/web-extension-did/app/web/hooks/referral.ts
+++ packages/web-extension-did/app/web/hooks/referral.ts
@@ -1,11 +1,16 @@
 import { useCurrentNetworkInfo } from '@portkey-wallet/hooks/hooks-ca/network';
 import { useReferral } from '@portkey-wallet/hooks/hooks-ca/referral';
 import { useCallback } from 'react';
+import singleMessage from 'utils/singleMessage';
 
 export const useClickReferral = () => {
   const { setViewReferralStatusStatus, referralLink } = useReferral();
   const currentNetworkInfo = useCurrentNetworkInfo();
   return useCallback(() => {
+    if (!referralLink) {
+      singleMessage.info('loading...');
+      return;
+    }
     setViewReferralStatusStatus();
     const url = `${currentNetworkInfo?.referralUrl}/referral?shortLink=${referralLink}`;
     const openWinder = window.open(url, '_blank');

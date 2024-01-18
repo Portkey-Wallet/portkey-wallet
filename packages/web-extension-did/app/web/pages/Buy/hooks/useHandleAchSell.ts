ebcfc7b22371d43cd620ec4cd4dbefb06ffc7ae2
ykx <yangkexin@portkey.finance>
2024-01-18 21:23:09 +0800

    fix: 🐛 sell json parse error

--- packages/web-extension-did/app/web/pages/Buy/hooks/useHandleAchSell.ts
+++ packages/web-extension-did/app/web/pages/Buy/hooks/useHandleAchSell.ts
@@ -15,6 +15,7 @@ import { ELF_SYMBOL } from '@portkey-wallet/constants/constants-ca/assets';
 import getSeed from 'utils/getSeed';
 import { chromeStorage } from 'store/utils';
 import singleMessage from 'utils/singleMessage';
+import { StorageKeyType } from 'utils/storage/storage';
 
 export const useHandleAchSell = () => {
   const { setLoading } = useLoading();
@@ -38,7 +39,20 @@ export const useHandleAchSell = () => {
       if (!aelfToken) throw new Error('Sell Transfer: No Token');
       const manager = getWallet(privateKey);
       if (!manager?.keyPair) throw new Error('Sell Transfer: No keyPair');
-      const guardiansApprovedStr = await chromeStorage.getItem('portkeyOffRampGuardiansApproveList');
+      const guardiansApprovedStr = await chromeStorage.getItem(
+        `RampSellApproveList_${params.orderId}` as StorageKeyType,
+      );
+      let guardiansApprovedParse;
+      try {
+        guardiansApprovedParse =
+          typeof guardiansApprovedStr === 'string' && guardiansApprovedStr.length > 0
+            ? JSON.parse(guardiansApprovedStr)
+            : undefined;
+      } catch (error) {
+        console.log('json parse error');
+        guardiansApprovedParse = undefined;
+      }
+
       const rawResult = await getTransactionRaw({
         contractAddress: chainInfo.caContractAddress,
         rpcUrl: chainInfo?.endPoint || '',
@@ -54,13 +68,13 @@ export const useHandleAchSell = () => {
             to: `ELF_${params.address}_AELF`,
             amount: timesDecimals(params.cryptoAmount, aelfToken.decimals).toNumber(),
           },
-          guardiansApproved: JSON.parse(guardiansApprovedStr || ''),
+          guardiansApproved: guardiansApprovedParse,
         },
       });
       if (!rawResult || !rawResult.result) {
         throw new Error('Failed to get raw transaction.');
       }
-      await chromeStorage.removeItem('portkeyOffRampGuardiansApproveList');
+      await chromeStorage.removeItem(`RampSellApproveList_${params.orderId}`);
       const publicKey = manager.keyPair.getPublic('hex');
       const message = SparkMD5.hash(`${params.orderId}${rawResult.result.data}`);
       const signature = AElf.wallet.sign(Buffer.from(message).toString('hex'), manager.keyPair).toString('hex');

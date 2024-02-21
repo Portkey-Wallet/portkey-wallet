7bb85b152376579d2016eafa9dfd2a6941b18898
sarah-portkey <sarah@portkey.finance>
2024-02-21 16:07:59 +0800

    feat: 🎸 popup open loading

---
+++ packages/web-extension-did/app/web/popup-load.js
@@ -0,0 +1,15 @@
+(() => {
+  const script = document.createElement('script');
+  script.src = './js/popup.js';
+  script.addEventListener('load', () => {
+    const root = document.getElementById('root');
+    if (root) {
+      root.style.height = 'auto';
+      root.style.width = 'auto';
+    }
+  });
+  const timer = setTimeout(() => {
+    clearTimeout(timer);
+    document.body.appendChild(script);
+  }, 1000);
+})();

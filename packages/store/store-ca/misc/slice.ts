b31e2c649d185447174224828990ee8d4acfcd5d
sarah-portkey <sarah@portkey.finance>
2024-03-01 16:52:28 +0800

    feat: 🎸 add sideChainTokenReceiveTip store & hooks

--- packages/store/store-ca/misc/slice.ts
+++ packages/store/store-ca/misc/slice.ts
@@ -2,15 +2,29 @@ import { createSlice } from '@reduxjs/toolkit';
 import { getPhoneCountryCode, setLocalPhoneCountryCodeAction } from './actions';
 import { MiscState } from './types';
 import { DefaultCountry } from '@portkey-wallet/constants/constants-ca/country';
+import { NetworkType } from '@portkey-wallet/types';
 
 const initialState: MiscState = {
   phoneCountryCodeListChainMap: {},
   defaultPhoneCountryCode: DefaultCountry,
+  sideChainTokenReceiveTipMap: {
+    MAINNET: true,
+    TESTNET: true,
+  },
 };
 export const miscSlice = createSlice({
   name: 'misc',
   initialState,
-  reducers: {},
+  reducers: {
+    setSideChainTokenReceiveTipMap: (state, { payload }: { payload: { network: NetworkType; value: boolean } }) => {
+      const { network, value } = payload;
+      state.sideChainTokenReceiveTipMap = {
+        ...state.sideChainTokenReceiveTipMap,
+        [network]: value,
+      };
+    },
+    resetMisc: () => initialState,
+  },
   extraReducers: builder => {
     builder
       .addCase(getPhoneCountryCode.fulfilled, (state, action) => {
@@ -28,3 +42,5 @@ export const miscSlice = createSlice({
       });
   },
 });
+
+export const { setSideChainTokenReceiveTipMap, resetMisc } = miscSlice.actions;

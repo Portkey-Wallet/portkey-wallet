beee0a2ec36e44a7233ea64221ecbf6af9afb76a
portkey-yellow <yellow.m@portkey.finance>
2024-03-13 13:41:23 +0800

    fix: 🐛 balance

--- packages/mobile-app-did/js/utils/aelfUtils.ts
+++
@@ -1,8 +0,0 @@
-import AElf from 'aelf-sdk';
-import { sleep } from '@portkey-wallet/utils';
-const { wallet: Wallet } = AElf;
-const wallet = Wallet.getWalletByPrivateKey('28805dd286a972f0ff268ba42646d5d952d770141bfec55c98e10619c268ecea');
-
-export function getDefaultWallet() {
-  return wallet;
-}

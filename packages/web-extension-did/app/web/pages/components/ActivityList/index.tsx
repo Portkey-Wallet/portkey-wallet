d3b8235c4d607cf08cc7e39ed94eff7e18b7d088
sarah-portkey <sarah@portkey.finance>
2024-04-07 16:01:58 +0800

    feat: 🎸 activity ui

--- packages/web-extension-did/app/web/pages/components/ActivityList/index.tsx
+++ packages/web-extension-did/app/web/pages/components/ActivityList/index.tsx
@@ -1,28 +1,32 @@
 import { SHOW_FROM_TRANSACTION_TYPES, TransactionTypes } from '@portkey-wallet/constants/constants-ca/activity';
 import { ActivityItemType, the2ThFailedActivityItemType } from '@portkey-wallet/types/types-ca/activity';
-import { AmountSign, formatWithCommas, formatStr2EllipsisStr } from '@portkey-wallet/utils/converter';
+import {
+  AmountSign,
+  formatWithCommas,
+  formatStr2EllipsisStr,
+  formatAmountUSDShow,
+} from '@portkey-wallet/utils/converter';
 import { List } from 'antd-mobile';
 import CustomSvg from 'components/CustomSvg';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useNavigateState } from 'hooks/router';
 import './index.less';
 import LoadingMore from 'components/LoadingMore/LoadingMore';
-import { transNetworkText } from '@portkey-wallet/utils/activity';
 import { Button, Modal } from 'antd';
 import { useAppCASelector } from '@portkey-wallet/hooks/hooks-ca';
-import { formatTransferTime } from '@portkey-wallet/utils/time';
+import { formatTransferTime, isSameDay } from '@portkey-wallet/utils/time';
 import { useTranslation } from 'react-i18next';
 import { intervalCrossChainTransfer } from 'utils/sandboxUtil/crossChainTransfer';
 import { useAppDispatch, useLoading } from 'store/Provider/hooks';
 import { removeFailedActivity } from '@portkey-wallet/store/store-ca/activity/slice';
-import { useCurrentChainList, useDefaultToken } from '@portkey-wallet/hooks/hooks-ca/chainList';
+import { useCurrentChainList } from '@portkey-wallet/hooks/hooks-ca/chainList';
 import { addressFormat } from '@portkey-wallet/utils';
-import { useFreshTokenPrice, useAmountInUsdShow } from '@portkey-wallet/hooks/hooks-ca/useTokensPrice';
+import { useFreshTokenPrice } from '@portkey-wallet/hooks/hooks-ca/useTokensPrice';
 import { BalanceTab } from '@portkey-wallet/constants/constants-ca/assets';
 import { useCurrentNetworkInfo, useIsMainnet } from '@portkey-wallet/hooks/hooks-ca/network';
-import { ChainId } from '@portkey/provider-types';
 import getSeed from 'utils/getSeed';
 import { ITransactionLocationState } from 'types/router';
+import clsx from 'clsx';
 
 export interface IActivityListProps {
   data?: ActivityItemType[];
@@ -39,11 +43,10 @@ export default function ActivityList({ data, chainId, hasMore, loadMore }: IActi
   const dispatch = useAppDispatch();
   const chainList = useCurrentChainList();
   useFreshTokenPrice();
-  const amountInUsdShow = useAmountInUsdShow();
-  const defaultToken = useDefaultToken(chainId ? (chainId as ChainId) : undefined);
+  const currentNetwork = useCurrentNetworkInfo();
 
   const activityListLeftIcon = (type: TransactionTypes) => {
-    return SHOW_FROM_TRANSACTION_TYPES.includes(type) ? 'Transfer' : 'socialRecovery';
+    return SHOW_FROM_TRANSACTION_TYPES.includes(type) ? 'Transfer' : 'SystemActivity';
   };
 
   const nav = useNavigateState<ITransactionLocationState>();
@@ -52,58 +55,6 @@ export default function ActivityList({ data, chainId, hasMore, loadMore }: IActi
       nav('/transaction', { state: { item, chainId, previousPage: chainId ? '' : BalanceTab.ACTIVITY } });
     },
     [chainId, nav],
-  );
-
-  const amountOrIdUI = (item: ActivityItemType) => {
-    const { transactionName, isReceived, amount, symbol, nftInfo, decimals } = item;
-    const sign = isReceived ? AmountSign.PLUS : AmountSign.MINUS;
-    return (
-      <p className="row-1 flex-row-between">
-        <span className="row-1-left flex-row-between">
-          <span>{transactionName}</span>
-          {nftInfo?.nftId && <span className="nft-id-show">#{nftInfo.nftId}</span>}
-          {!nftInfo?.nftId && (
-            <span className="amount-show">{`${formatWithCommas({
-              sign,
-              amount,
-              decimals,
-              digits: Number(decimals),
-            })}`}</span>
-          )}
-        </span>
-        {!nftInfo?.nftId && symbol && <span className="amount-symbol">{symbol}</span>}
-      </p>
-    );
-  };
-
-  const currentNetwork = useCurrentNetworkInfo();
-  const fromAndUsdUI = useCallback(
-    (item: ActivityItemType) => {
-      const { fromAddress, fromChainId, decimals, amount, symbol, nftInfo } = item;
-      const transFromAddress = addressFormat(fromAddress, fromChainId, currentNetwork.walletType);
-
-      return (
-        <p className="row-2 flex-between">
-          <span>{`From: ${formatStr2EllipsisStr(transFromAddress, [7, 4])}`}</span>
-          {nftInfo?.nftId && <span className="nft-name">{formatStr2EllipsisStr(nftInfo.alias)}</span>}
-          {isMainnet && !nftInfo?.nftId && (
-            <span className="convert">{amountInUsdShow(amount, decimals || defaultToken.decimals, symbol)}</span>
-          )}
-        </p>
-      );
-    },
-    [amountInUsdShow, currentNetwork.walletType, defaultToken.decimals, isMainnet],
-  );
-
-  const networkUI = useCallback(
-    (item: ActivityItemType) => {
-      const { fromChainId, toChainId } = item;
-      const from = transNetworkText(fromChainId, !isMainnet);
-      const to = transNetworkText(toChainId, !isMainnet);
-
-      return <p className="row-3">{`${from}->${to}`}</p>;
-    },
-    [isMainnet],
   );
 
   const showErrorModal = useCallback(
@@ -161,12 +112,12 @@ export default function ActivityList({ data, chainId, hasMore, loadMore }: IActi
     [activity.failedActivityMap, retryCrossChain],
   );
 
-  const resendUI = useCallback(
+  const renderResendBtn = useCallback(
     (item: ActivityItemType) => {
       if (!activity.failedActivityMap[item.transactionId]) return;
 
       return (
-        <div className="row-4">
+        <div className="activity-item-resend-btn">
           <Button type="primary" className="resend-btn" onClick={(e) => handleResend(e, item.transactionId)}>
             Resend
           </Button>
@@ -176,59 +127,148 @@ export default function ActivityList({ data, chainId, hasMore, loadMore }: IActi
     [activity.failedActivityMap, handleResend],
   );
 
-  const notShowFromAndNetworkUI = useCallback(
+  const dividerUI = useMemo(() => <div className="divider-center" />, []);
+
+  const renderActivityTitle = useCallback(
     (item: ActivityItemType) => {
-      const { isReceived, amount, symbol, decimals } = item;
-      const sign = isReceived ? AmountSign.PLUS : AmountSign.MINUS;
+      const { transactionName, transactionType, isReceived, fromAddress, fromChainId, toAddress, toChainId } = item;
+      const transAddress = isReceived
+        ? addressFormat(fromAddress, fromChainId, currentNetwork.walletType)
+        : addressFormat(toAddress, toChainId, currentNetwork.walletType);
+      const showAddress = `${isReceived ? 'From' : 'To'} ${formatStr2EllipsisStr(transAddress, [8, 9])}`;
       return (
-        <div className="right right-not-from">
-          <span>{item?.transactionName}</span>
-          <div className="right-not-from-amount">
-            <div>{`${formatWithCommas({ sign, amount, decimals, digits: 4 })} ${symbol ?? ''}`}</div>
-            {isMainnet && (
-              <div className="usd">{amountInUsdShow(amount, decimals || defaultToken.decimals, symbol)}</div>
-            )}
-          </div>
+        <div className="activity-item-title">
+          <div className="transaction-name">{transactionName}</div>
+          <div className="transaction-address">{showAddress}</div>
+          {TransactionTypes.CROSS_CHAIN_TRANSFER === transactionType && (
+            <div className="cross-chain-transfer">Cross Chain Transfer</div>
+          )}
         </div>
       );
     },
-    [amountInUsdShow, defaultToken.decimals, isMainnet],
+    [currentNetwork.walletType],
   );
+
+  const renderActivityAmount = useCallback(
+    (item: ActivityItemType) => {
+      const { isReceived, amount, decimals, symbol, currentTxPriceInUsd, nftInfo } = item;
+      const sign = isReceived ? AmountSign.PLUS : AmountSign.MINUS;
+      return (
+        <div className="activity-item-amount">
+          <div className={clsx('transaction-amount', isReceived && 'received-amount')}>
+            <span className="amount-show">{`${formatWithCommas({
+              sign,
+              amount,
+              decimals,
+              digits: Number(decimals),
+            })}`}</span>
+            {!nftInfo?.nftId && symbol && <span className="amount-symbol">{` ${symbol}`}</span>}
+          </div>
+          {nftInfo?.nftId ? (
+            <div className="transaction-nft-symbol">{nftInfo.alias}</div>
+          ) : (
+            <div className={clsx('transaction-convert', !isMainnet && 'hidden-transaction-convert')}>
+              {formatAmountUSDShow(currentTxPriceInUsd ?? 0)}
+            </div>
+          )}
+        </div>
+      );
+    },
+    [isMainnet],
+  );
+
+  const renderSystemActivityItem = useCallback((item: ActivityItemType) => {
+    return (
+      <div className="activity-item-system-detail">
+        <span>{item?.transactionName}</span>
+      </div>
+    );
+  }, []);
+
+  const renderActivityIcon = useCallback((item: ActivityItemType) => {
+    return item.listIcon ? (
+      <div
+        className="custom-list-icon"
+        style={{
+          backgroundImage: `url(${item.listIcon})`,
+        }}
+      />
+    ) : (
+      <CustomSvg type={activityListLeftIcon(item.transactionType)} />
+    );
+  }, []);
+
+  const renderActivityItem = useCallback(
+    (item: ActivityItemType, index: number) => (
+      <List.Item key={`activityList_${item.transactionId}_${index}`}>
+        <div
+          className={clsx(
+            'activity-item',
+            'flex-column',
+            activity.failedActivityMap[item.transactionId] && 'activity-item-resend',
+          )}>
+          <div className="activity-item-content flex-row-center" onClick={() => navToDetail(item)}>
+            {renderActivityIcon(item)}
+            {item.isSystem ? (
+              renderSystemActivityItem(item)
+            ) : (
+              <div className="activity-item-detail flex-between-center">
+                {renderActivityTitle(item)}
+                {renderActivityAmount(item)}
+              </div>
+            )}
+          </div>
+          {renderResendBtn(item)}
+        </div>
+      </List.Item>
+    ),
+    [
+      activity.failedActivityMap,
+      renderActivityIcon,
+      renderSystemActivityItem,
+      renderActivityTitle,
+      renderActivityAmount,
+      renderResendBtn,
+      navToDetail,
+    ],
+  );
+
+  const renderActivityList = useMemo(() => {
+    return (
+      <List>
+        {data?.map((item, index) => {
+          if (index === 0) {
+            return (
+              <>
+                <div key={`activityList_date_${index}`} className="activity-date-item">
+                  {formatTransferTime(item.timestamp)}
+                </div>
+                {renderActivityItem(item, index)}
+              </>
+            );
+          } else {
+            if (isSameDay(data[index - 1].timestamp, item.timestamp)) {
+              return renderActivityItem(item, index);
+            } else {
+              return (
+                <>
+                  <div key={`activityList_date_${index}`} className="activity-date-item">
+                    {dividerUI}
+                    {formatTransferTime(item.timestamp)}
+                  </div>
+                  {renderActivityItem(item, index)}
+                </>
+              );
+            }
+          }
+        })}
+      </List>
+    );
+  }, [data, dividerUI, renderActivityItem]);
 
   return (
     <div className="activity-list">
-      <List>
-        {data?.map((item, index) => (
-          <List.Item key={`activityList_${item.transactionId}_${index}`}>
-            <div className="activity-item" onClick={() => navToDetail(item)}>
-              <div className="time">{formatTransferTime(item.timestamp)}</div>
-              <div className="info">
-                {!!item.listIcon && (
-                  <div
-                    className="custom-list-icon"
-                    style={{
-                      backgroundImage: `url(${item.listIcon})`,
-                    }}
-                  />
-                )}
-                {!item.listIcon && <CustomSvg type={activityListLeftIcon(item.transactionType)} />}
-
-                {/* [Transfer, CrossChainTransfer, ClaimToken] display the network and from UI */}
-                {!SHOW_FROM_TRANSACTION_TYPES.includes(item.transactionType) ? (
-                  notShowFromAndNetworkUI(item)
-                ) : (
-                  <div className="right">
-                    {amountOrIdUI(item)}
-                    {fromAndUsdUI(item)}
-                    {networkUI(item)}
-                    {resendUI(item)}
-                  </div>
-                )}
-              </div>
-            </div>
-          </List.Item>
-        ))}
-      </List>
+      {renderActivityList}
       <LoadingMore hasMore={hasMore} loadMore={loadMore} className="load-more" />
     </div>
   );

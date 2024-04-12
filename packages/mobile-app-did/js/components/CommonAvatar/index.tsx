3046f954cb78d996c4ad5fb0b122716cee917a64
thomas-portkey <thomas@portkey.finance>
2024-04-12 15:02:41 +0800

    feat: 🎸 add Image Preview

--- packages/mobile-app-did/js/components/CommonAvatar/index.tsx
+++ packages/mobile-app-did/js/components/CommonAvatar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Svg, { IconName } from 'components/Svg';
 import { pTd } from 'utils/unit';
 import { Text, TextStyle, View, ViewStyle } from 'react-native';
@@ -44,7 +44,6 @@ export default function CommonAvatar(props: CommonAvatarProps) {
     borderStyle: borderStyleProp,
   } = props;
 
-  const [loadError, setLoadError] = useState(false);
   const initialsTitle = String(title?.[0] || '').toUpperCase();
 
   const sizeStyle = useMemo(
@@ -76,7 +75,28 @@ export default function CommonAvatar(props: CommonAvatarProps) {
   );
 
   // when change url ,reset loading error state
-  useEffect(() => setLoadError(false), [imageUrl]);
+  useEffect(() => setIsImageLoad(false), [imageUrl]);
+
+  const [isImageLoad, setIsImageLoad] = useState(false);
+
+  const renderLabelDom = useCallback(
+    (renderStyle?: ViewStyle) => {
+      return (
+        <View
+          style={[
+            styles.avatarWrap,
+            shapeType === 'square' && styles.squareStyle,
+            hasBorder && borderStyle,
+            sizeStyle,
+            style,
+            renderStyle,
+          ]}>
+          <Text style={titleStyle}>{initialsTitle}</Text>
+        </View>
+      );
+    },
+    [borderStyle, hasBorder, initialsTitle, shapeType, sizeStyle, style, titleStyle],
+  );
 
   if (svgName)
     return (
@@ -93,8 +113,8 @@ export default function CommonAvatar(props: CommonAvatarProps) {
       />
     );
 
-  if (imageUrl && !loadError) {
-    return checkIsSvgUrl(imageUrl) ? (
+  if (imageUrl && checkIsSvgUrl(imageUrl)) {
+    return (
       <View style={[styles.avatarWrap, shapeType === 'square' && styles.squareStyle, sizeStyle, style]}>
         <SvgCssUri
           uri={imageUrl}
@@ -103,30 +123,37 @@ export default function CommonAvatar(props: CommonAvatarProps) {
           preserveAspectRatio={preserveAspectRatio}
         />
       </View>
-    ) : (
-      <FastImage
-        resizeMode={resizeMode}
-        style={[styles.avatarWrap, shapeType === 'square' && styles.squareStyle, sizeStyle, style as any]}
-        onError={() => setLoadError(true)}
-        source={{
-          uri: imageUrl,
-        }}
-      />
     );
   }
 
-  return (
-    <View
-      style={[
-        styles.avatarWrap,
-        shapeType === 'square' && styles.squareStyle,
-        hasBorder && borderStyle,
-        sizeStyle,
-        style,
-      ]}>
-      <Text style={titleStyle}>{initialsTitle}</Text>
-    </View>
-  );
+  if (imageUrl) {
+    return (
+      <View>
+        <FastImage
+          resizeMode={resizeMode}
+          style={[
+            styles.avatarWrap,
+            shapeType === 'square' && styles.squareStyle,
+            sizeStyle,
+            style as any,
+            !isImageLoad && styles.fastImageLoading,
+          ]}
+          onLoad={() => {
+            setIsImageLoad(true);
+          }}
+          source={{
+            uri: imageUrl,
+          }}
+        />
+        {!isImageLoad &&
+          renderLabelDom({
+            position: 'absolute',
+          })}
+      </View>
+    );
+  }
+
+  return renderLabelDom();
 }
 const styles = StyleSheet.create({
   avatarWrap: {
@@ -144,5 +171,8 @@ const styles = StyleSheet.create({
     backgroundColor: defaultColors.bg7,
     borderWidth: 0,
     color: defaultColors.font7,
+  },
+  fastImageLoading: {
+    opacity: 0,
   },
 });

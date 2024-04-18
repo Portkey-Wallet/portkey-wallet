32ee660c4a503f3af87546b2a5408d2c9d1d2664
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-04-18 11:29:41 +0800

    feat: change comments

--- packages/rn-core/router/provider.tsx
+++ packages/rn-core/router/provider.tsx
@@ -6,70 +6,16 @@ import { PortkeyEntries } from './types';
 import { useCredentials } from '@portkey-wallet/rn-base/hooks/store';
 import { useCurrentWalletInfo } from '@portkey-wallet/hooks/hooks-ca/wallet';
 import { sleep } from '@portkey-wallet/utils';
-import navigationService, { useFocusEffect, useNavigation } from '@portkey-wallet/rn-inject-sdk';
-import { AppRouteName, isNeedPinPage, mapRoute } from './map';
+import navigationService, { useNavigation } from '@portkey-wallet/rn-inject-sdk';
+import { isNeedPinPage, mapRoute } from './map';
 import CommonToast from '@portkey-wallet/rn-components/components/CommonToast';
-import useLatestIsFocusedRef from '@portkey-wallet/rn-base/hooks/useLatestIsFocusedRef';
 
 const RouterProvider = ({ children, value }: { children: any; value?: RouterParams }) => {
   const hasRun = useRef(false);
-  // const isNeedPinRoute = useRef(isNeedPinPage(value?.from as PortkeyEntries));
-  // const isSecurityLock = useRef(value?.from === mapRoute('SecurityLock'));
-  // const [isInteractionDisabled, setIsInteractionDisabled] = useState(false);
-  // const isFocused = useLatestIsFocusedRef();
-  // console.log('isFocused', isFocused, 'from', value?.from);
   if (!hasRun.current) {
     initRouter(value);
     hasRun.current = true;
   }
-
-  // const credentials = useCredentials();
-  // const wallet = useCurrentWalletInfo();
-  // const { address, caHash } = wallet;
-  // console.log('wfs data', 'wallet', wallet, 'credentials', credentials);
-  // const init = useCallback(async () => {
-  //   try {
-  //     if (address) {
-  //       // have pin, do nothing
-  //       if (credentials && caHash) {
-  //         console.log('有钱包!->已解锁', value?.from, 'isNeedPinRoute.current', isNeedPinRoute.current);
-  //         // login/signUp/scanCode...，go back.
-  //         if (!isNeedPinRoute.current) {
-  //           setIsInteractionDisabled(true);
-  //           CommonToast.success('Already Login!');
-  //           await sleep(800);
-  //           console.log('wfs goBack 17');
-  //           navigationService.goBack();
-  //           return;
-  //         }
-  //       } else {
-  //         console.log('有钱包!->未解锁');
-  //         // no pin, goto security lock.
-  //         if (isNeedPinRoute.current) {
-  //           console.log('goto SecurityLock 2');
-  //           navigationService.reset('SecurityLock');
-  //         }
-  //       }
-  //     } else {
-  //       console.log('无钱包!');
-  //       // no wallet, so goto login page
-  //       // (but signUp page/scanQrcode page don't need goto login page)
-  //       if (isNeedPinRoute.current) {
-  //         navigationService.reset('LoginPortkey');
-  //       }
-  //     }
-  //   } catch (e) {
-  //     console.log('route init', e);
-  //   }
-  // }, [address, credentials, caHash]);
-  // useEffect(() => {
-  //   init();
-  //   // set current route name for navigationService instance
-  //   navigationService.setRouteName(value?.from);
-  //   return () => {
-  //     navigationService.setRouteName(router.peek()?.name);
-  //   };
-  // }, [init]);
 
   return (
     <RouterContext.Provider value={value ?? defaultRouterParams}>
@@ -88,13 +34,12 @@ function BackHandler({ children }: { children: any }) {
   const credentials = useCredentials();
   const wallet = useCurrentWalletInfo();
   const { address, caHash } = wallet;
-  // console.log('wfs data', 'wallet', wallet, 'credentials', credentials);
   const init = useCallback(async () => {
     try {
       if (address) {
         // have pin, do nothing
         if (credentials && caHash) {
-          console.log('有钱包!->已解锁', 'from', from, 'router pages', router.pages);
+          console.log('wallet!->unlocked', 'from', from, 'router pages', router.pages);
           // login/signUp/scanCode...，go back.
           if (!isNeedPinRoute.current) {
             setIsInteractionDisabled(true);
@@ -108,7 +53,7 @@ function BackHandler({ children }: { children: any }) {
             return;
           }
         } else {
-          console.log('有钱包!->未解锁', 'from', from, 'router pages', router.pages);
+          console.log('wallet!->locked', 'from', from, 'router pages', router.pages);
           // no pin, goto security lock.
           if (isNeedPinRoute.current || isLoginPage.current) {
             console.log('goto SecurityLock 2');
@@ -116,7 +61,7 @@ function BackHandler({ children }: { children: any }) {
           }
         }
       } else {
-        console.log('无钱包!');
+        console.log('no wallet', 'from', from, 'router pages', router.pages);
         // no wallet, so goto login page
         // (but signUp page/scanQrcode page don't need goto login page)
         if (isNeedPinRoute.current) {
@@ -129,30 +74,17 @@ function BackHandler({ children }: { children: any }) {
   }, [address, credentials, caHash]);
   useEffect(() => {
     // set current route name for navigationService instance
-    console.log('初始化时机', 'from', from, 'router pages', router.pages);
+    console.log('init timing', 'from', from, 'router pages', router.pages);
     if (navigation.isFocused()) {
-      console.log('初始化时机，设置当前route');
+      console.log('init timing, set current route');
       navigationService.setRouteName(from);
     }
     init();
     return () => {
-      console.log('销毁时机，设置当前route', 'from', from, 'router pages', router.pages);
+      console.log('destroy timing, set current route', 'from', from, 'router pages', router.pages);
       navigationService.setRouteName(router.peek()?.name);
     };
   }, [init]);
-  // usePreventHardwareBack();
-  // useHardwareBackPress(() => {
-  //   console.log('from', from, 'router', router.pages, 'router.listenersFunc()', router.listenersFunc());
-  //   // router.listenersFunc()[from]?.['blur'].forEach(item => item());
-  //   // router.pop();
-  //   console.log('from2', from, 'router2', router.pages, 'router.listenersFunc()', router.listenersFunc());
-  //   return false;
-  // });
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     console.log('wfs focus change', from);
-  //   }, []),
-  // );
   useEffect(() => {
     const unsubscribeFocus = navigation.addListener('focus', () => {
       console.log('wfs listener1 focus call', from);
@@ -172,9 +104,7 @@ function BackHandler({ children }: { children: any }) {
       {isInteractionDisabled && <View style={styles.overlay} pointerEvents="box-only" />}
     </View>
   );
-  // return <>{children}</>;
 }
-// useHardwareBackPress
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -188,6 +118,5 @@ function initRouter(value: { from: string; params: any } | undefined) {
   const currentPage = value?.from as PortkeyEntries;
   router.push({ name: currentPage, params: value?.params });
   console.log('wfs current route list', router.pages);
-  // router.listenersFunc()[currentPage]?.['focus'].forEach(item => item());
 }
 export default RouterProvider;

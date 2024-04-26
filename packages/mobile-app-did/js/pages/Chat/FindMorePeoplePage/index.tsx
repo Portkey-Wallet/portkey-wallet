824d7405d18a78bb9da31597b1cbf8b2ea7aa3d5
carbon-portkey <lve@portkey.finance>
2024-04-26 16:55:16 +0800

    fix: functional button controlled by CMS

--- packages/mobile-app-did/js/pages/Chat/FindMorePeoplePage/index.tsx
+++ packages/mobile-app-did/js/pages/Chat/FindMorePeoplePage/index.tsx
@@ -27,7 +27,6 @@ import OfficialChatGroup from '../components/OfficialChatGroup';
 const FindMorePeople = () => {
   const iptRef = useRef<TextInput>(null);
   useInputFocus(iptRef);
-  // const { userId = '' } = useCurrentUserInfo();
   const navToChatDetails = useJumpToChatDetails();
   const [keyword, setKeyword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -116,18 +115,6 @@ const FindMorePeople = () => {
       </View>
       {!debounceWord && (
         <>
-          {/* <View style={[GStyles.flexRow, GStyles.spaceBetween, GStyles.itemEnd, styles.portkeyIdWrap]}>
-            <View style={[GStyles.flex1, GStyles.paddingRight(16)]}>
-              <TextM>{`My Portkey ID : `}</TextM>
-              <TextM numberOfLines={1}>{userId}</TextM>
-            </View>
-            <Touchable onPress={() => copyText(userId || '')}>
-              <Svg icon="copy" size={pTd(16)} />
-            </Touchable>
-            <Touchable style={GStyles.marginLeft(pTd(16))} onPress={() => navigationService.navigate('ChatQrCodePage')}>
-              <Svg icon="chat-scan" size={pTd(16)} />
-            </Touchable>
-          </View> */}
           <InviteFriendsSection />
           <OfficialChatGroup />
         </>

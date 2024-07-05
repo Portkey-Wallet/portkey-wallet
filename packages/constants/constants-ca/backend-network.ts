1c4d91315a6322db97bacabbc49bea77575dfade
wade-portkey <codingwizard@portkey.finance>
2024-07-05 19:05:22 +0800

    feat: 🎸 add auto login whitelist

--- packages/constants/constants-ca/backend-network.ts
+++ packages/constants/constants-ca/backend-network.ts
@@ -186,6 +186,7 @@ export const BackEndNetWorkMap: {
     eTransferUrl: 'https://test.etransfer.exchange',
     awakenUrl: 'https://test.awaken.finance',
     schrodingerUrl: 'https://schrodingerai.com',
+    sgrSchrodingerUrl: 'https://sgr.schrodingerai.com',
   },
   'back-end-testnet': {
     name: 'aelf Testnet',
@@ -211,6 +212,7 @@ export const BackEndNetWorkMap: {
     },
     awakenUrl: 'https://test.awaken.finance',
     schrodingerUrl: 'https://schrodingerai.com',
+    sgrSchrodingerUrl: 'https://sgr.schrodingerai.com',
   },
   'back-end-mainnet': {
     name: 'aelf Mainnet',
@@ -241,6 +243,7 @@ export const BackEndNetWorkMap: {
     eTransferUrl: 'https://etransfer.exchange',
     awakenUrl: 'https://awaken.finance',
     schrodingerUrl: 'https://schrodingernft.ai/',
+    sgrSchrodingerUrl: 'https://sgr.schrodingernft.ai/',
   },
   'back-end-mainnet-v2': {
     name: 'aelf Mainnet',
@@ -273,5 +276,6 @@ export const BackEndNetWorkMap: {
     },
     awakenUrl: 'https://awaken.finance',
     schrodingerUrl: 'https://schrodingernft.ai',
+    sgrSchrodingerUrl: 'https://sgr.schrodingernft.ai',
   },
 };

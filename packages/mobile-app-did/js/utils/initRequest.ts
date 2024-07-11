44a71d13a53a27284761c5dd0b07876cc7e15621
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-07-11 18:29:34 +0800

    fix: 1. disable send button logic 2.add common header

--- packages/mobile-app-did/js/utils/initRequest.ts
+++ packages/mobile-app-did/js/utils/initRequest.ts
@@ -1,5 +1,7 @@
 import { request } from '@portkey-wallet/api/api-did';
 import { nativeApplicationVersion } from 'expo-application';
 import { PlatFormInHeader } from '@portkey-wallet/api/api-did/types';
+import { codePushOperator } from './update';
 
-request.set('headers', { version: `v${nativeApplicationVersion}`, platform: PlatFormInHeader.APP });
+const hotfixLabel = codePushOperator.localPackage?.label ?? 0;
+request.set('headers', { version: `v${nativeApplicationVersion}` + hotfixLabel, platform: PlatFormInHeader.APP });

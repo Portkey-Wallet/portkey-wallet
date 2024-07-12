81cde19f5289e5559b03185a59ba5fd9adf4255d
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-07-12 20:36:29 +0800

    feat: change platform header

--- packages/mobile-app-did/js/utils/initRequest.ts
+++ packages/mobile-app-did/js/utils/initRequest.ts
@@ -3,11 +3,19 @@ import { nativeApplicationVersion } from 'expo-application';
 import { PlatFormInHeader } from '@portkey-wallet/api/api-did/types';
 import { codePushOperator, parseLabel } from './update';
 import im from '@portkey-wallet/im';
+import { isIOS } from '@portkey-wallet/utils/mobile/device';
+import { Platform } from 'react-native';
 
 export const initRequest = async () => {
   const localPackage = await codePushOperator.initLocalPackage();
   const hotfixLabel = parseLabel(localPackage?.label) ?? 0;
-  request.set('headers', { version: `v${nativeApplicationVersion}.` + hotfixLabel, platform: PlatFormInHeader.APP });
+  request.set('headers', {
+    version: `v${nativeApplicationVersion}.` + hotfixLabel,
+    platform: isIOS ? PlatFormInHeader.IOS : Platform.OS === 'android' ? PlatFormInHeader.ANDROID : 'unknown',
+  });
   im.setHeader('version', `v${nativeApplicationVersion}.` + hotfixLabel);
-  im.setHeader('platform', PlatFormInHeader.APP);
+  im.setHeader(
+    'platform',
+    isIOS ? PlatFormInHeader.IOS : Platform.OS === 'android' ? PlatFormInHeader.ANDROID : 'unknown',
+  );
 };

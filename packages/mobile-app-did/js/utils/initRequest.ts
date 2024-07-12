1ac532ac9315bd5f0aca2951249d689d664ea37e
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-07-12 20:06:39 +0800

    fix: hotfix format

--- packages/mobile-app-did/js/utils/initRequest.ts
+++ packages/mobile-app-did/js/utils/initRequest.ts
@@ -1,12 +1,12 @@
 import { request } from '@portkey-wallet/api/api-did';
 import { nativeApplicationVersion } from 'expo-application';
 import { PlatFormInHeader } from '@portkey-wallet/api/api-did/types';
-import { codePushOperator } from './update';
+import { codePushOperator, parseLabel } from './update';
 import im from '@portkey-wallet/im';
 
 export const initRequest = async () => {
   const localPackage = await codePushOperator.initLocalPackage();
-  const hotfixLabel = localPackage?.label ?? 0;
+  const hotfixLabel = parseLabel(localPackage?.label) ?? 0;
   request.set('headers', { version: `v${nativeApplicationVersion}.` + hotfixLabel, platform: PlatFormInHeader.APP });
   im.setHeader('version', `v${nativeApplicationVersion}.` + hotfixLabel);
   im.setHeader('platform', PlatFormInHeader.APP);

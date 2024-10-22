825de25e1c372093c2b450d8bf431d3000b7dbfd
david <alex@portkey.finance>
2024-10-22 09:57:06 +0800

    feat: 🎸 change font style

--- packages/mobile-app-did/js/pages/Send/index.tsx
+++ packages/mobile-app-did/js/pages/Send/index.tsx
@@ -2,12 +2,14 @@ import SendHome from './SendHome';
 import SendPreview from './SendPreview';
 import SelectContact from './SelectContact';
 import SelectAsset from './SelectAsset';
+import SendFinishPage from './SendFinishPage';
 
 const stackNav = [
   { name: 'SendHome', component: SendHome },
   { name: 'SendPreview', component: SendPreview },
   { name: 'SelectContact', component: SelectContact },
   { name: 'SelectAsset', component: SelectAsset },
+  { name: 'SendFinishPage', component: SendFinishPage },
 ] as const;
 
 export default stackNav;

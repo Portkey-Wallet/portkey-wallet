99bc3731109e8d0db61d594cae3c319e47182990
peony <ethanael_s@portkey.finance>
2024-10-25 17:34:55 +0800

    refactor: move send components

--- packages/mobile-app-did/js/pages/Send/index.tsx
+++ packages/mobile-app-did/js/pages/Send/index.tsx
@@ -1,12 +1,12 @@
 import SendHome from './SendHome';
-import SendPreview from '../../components/SendReceivePreview';
+import SendReceivePreview from '../../components/SendReceivePreview';
 import SelectContact from './SelectContact';
 import SelectAsset from './SelectAsset';
 import SendReceiveFinishPage from '../../components/SendReceiveFinishPage';
 
 const stackNav = [
   { name: 'SendHome', component: SendHome },
-  { name: 'SendPreview', component: SendPreview },
+  { name: 'SendPreview', component: SendReceivePreview },
   { name: 'SelectContact', component: SelectContact },
   { name: 'SelectAsset', component: SelectAsset },
   { name: 'SendFinishPage', component: SendReceiveFinishPage },

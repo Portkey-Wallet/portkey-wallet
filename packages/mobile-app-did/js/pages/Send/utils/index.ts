18e1e48c6dcf547c1500a64edb15f6390054b8d6
david <alex@portkey.finance>
2024-10-30 18:35:03 +0800

    refactor: 💡 send preview

--- packages/mobile-app-did/js/pages/Send/utils/index.ts
+++ packages/mobile-app-did/js/pages/Send/utils/index.ts
@@ -2,6 +2,8 @@ import { request } from '@portkey-wallet/api/api-did';
 import { ZERO } from '@portkey-wallet/constants/misc';
 import { ChainId } from '@portkey-wallet/types';
 import { Linking } from 'react-native';
+import { INetworkItem } from '../components/SelectNetwork';
+import { TransferType } from '@portkey-wallet/types/types-ca/routeParams';
 
 export interface IGetSendNetworkListParams {
   symbol: string;
@@ -31,3 +33,14 @@ export function getSmallerValue(v1: string, v2: string) {
 export function getLimitTips(symbol: string, from: string, to: string) {
   return `Transfer limit: ${from} to ${to} ${symbol}`;
 }
+
+export const getEstimatedTime = (targetNetwork: INetworkItem, transferType: TransferType) => {
+  const transferItem = targetNetwork?.serviceList?.find(ele =>
+    ele?.serviceName?.toLocaleLowerCase()?.includes('transfer'),
+  );
+  const bridgeItem = targetNetwork?.serviceList?.find(ele => ele?.serviceName?.toLocaleLowerCase()?.includes('bridge'));
+
+  if (transferType === TransferType.E_TRANSFER) return transferItem?.multiConfirmTime;
+  if (transferType === TransferType.E_BRIDGE) return bridgeItem?.multiConfirmTime;
+  return '';
+};

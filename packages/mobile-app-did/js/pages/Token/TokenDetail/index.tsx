61fd4ed8361a3a57d7ed44301e7f247eaa6da9a6
wade-portkey <codingwizard@portkey.finance>
2024-11-16 17:43:30 +0800

    fix: 🐛 fix token detail top tab

--- packages/mobile-app-did/js/pages/Token/TokenDetail/index.tsx
+++ packages/mobile-app-did/js/pages/Token/TokenDetail/index.tsx
@@ -24,21 +24,17 @@ const TokenDetail: React.FC = () => {
     if (!tokenSection.tokens || tokenSection.tokens.length < 1) {
       return <NoData noPic message={t('No Data')} />;
     }
-    if (tokenSection.tokens?.length === 1) {
-      return <TokenDetailPage tokenInfo={tokenSection.tokens[0]} tokenSection={tokenSection} />;
-    } else {
-      return (
-        <TokenDetailTopTab
-          initialRouteName={tokenSection.tokens[index].displayChainName}
-          tabList={tokenSection.tokens.map(token => {
-            return {
-              name: token.displayChainName ?? '',
-              tabItemDom: <TokenDetailPage tokenInfo={token} tokenSection={tokenSection} />,
-            };
-          })}
-        />
-      );
-    }
+    return (
+      <TokenDetailTopTab
+        initialRouteName={tokenSection.tokens[index].displayChainName}
+        tabList={tokenSection.tokens.map(token => {
+          return {
+            name: token.displayChainName ?? '',
+            tabItemDom: <TokenDetailPage tokenInfo={token} tokenSection={tokenSection} />,
+          };
+        })}
+      />
+    );
   }, [index, t, tokenSection]);
 
   return (

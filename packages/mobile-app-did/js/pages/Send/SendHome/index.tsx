d93eb96b0c47112c1f17e3f3f9755ca287d27ba0
nathanwilliams-hi <nathan.williams@portkey.finance>
2024-11-18 13:02:57 +0800

    feat: refresh recent contact list and switch component disable style

--- packages/mobile-app-did/js/pages/Send/SendHome/index.tsx
+++ packages/mobile-app-did/js/pages/Send/SendHome/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { KeyboardAvoidingView, Platform, View } from 'react-native';
 import PageContainer from 'components/PageContainer';
 import navigationService from 'utils/navigationService';
@@ -137,6 +137,7 @@ const SendHome: React.FC = () => {
   const [sendUsdNumber, setSendUsdNumber] = useState<string>(''); // tokenNumber  like 100
   const debounceSendNumber = useDebounce(sendNumber, 500);
   const [maxAmountSend, setMaxAmountSend] = useState<string>('0');
+  const getListFnRef = useRef<() => void>();
   const maxAmountSendUsd = useMemo(
     () => ZERO.plus(maxAmountSend).times(tokenPriceObject[assetInfo.symbol]).toFixed(2),
     [assetInfo.symbol, maxAmountSend, tokenPriceObject],
@@ -352,18 +353,17 @@ const SendHome: React.FC = () => {
     setRecentList(reList || []);
   }, [assetInfo.chainId, assetInfo.symbol, getTransformedRecentList, sendType]);
 
-  useEffectOnce(() => {
+  const initList = useCallback(() => {
     initBookList();
-    initBookList();
-  });
-
-  useEffectOnce(() => {
     initSavedList();
+  }, [initBookList, initSavedList]);
+  getListFnRef.current = initList;
+  useEffectOnce(() => {
+    initList();
   });
   useEffectOnce(() => {
     const listener = myEvents.updateSendAddressList.addListener(() => {
-      // todo:
-      console.log('update list');
+      getListFnRef.current?.();
     });
     return () => {
       listener.remove();

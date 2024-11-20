124cd4701fd3edeee36ce3ae9eeb25c055ea752c
aron <aron@portkey.finance>
2024-11-20 19:17:09 +0800

    feat: fix ui bug

--- packages/mobile-app-did/js/components/TabsDrawer/components/TabsDom/index.tsx
+++ packages/mobile-app-did/js/components/TabsDrawer/components/TabsDom/index.tsx
@@ -60,6 +60,8 @@ function TabsDom({ activeWebViewRef, clickBottomActionBtn }: IProps) {
   });
 
   const handleNaviagte = useCallback(() => {
+    console.log('1111111111111111111');
+
     navigationService.navigate('DiscoverSearch', { address: activeItem?.url });
     dispatch(changeDrawerOpenStatus(false));
   }, [activeItem?.url, dispatch]);

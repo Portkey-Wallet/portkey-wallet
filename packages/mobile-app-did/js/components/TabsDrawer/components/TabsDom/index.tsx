dbb09e90711ea9f90b39b6d014d8e585554d1125
Leon Zhang <nickooo@portkey.finance>
2024-11-22 11:29:39 +0800

    fix: fixed bugs of discover

--- packages/mobile-app-did/js/components/TabsDrawer/components/TabsDom/index.tsx
+++ packages/mobile-app-did/js/components/TabsDrawer/components/TabsDom/index.tsx
@@ -53,9 +53,11 @@ function TabsDom({ activeWebViewRef, clickBottomActionBtn }: IProps) {
   const { bookmarkList, refresh } = useBookmarkList();
   const isBookmarkLoading = useRef(false);
   const [tabStateMap, setTabStateMap] = useState<{
+    url: string;
     canGoBack: Record<string, boolean>;
     canGoForward: Record<string, boolean>;
   }>({
+    url: activeItem?.url || '',
     canGoBack: {},
     canGoForward: {},
   });
@@ -174,8 +176,10 @@ function TabsDom({ activeWebViewRef, clickBottomActionBtn }: IProps) {
     const canGoForward: boolean = tabStateMap?.canGoForward?.[String(ele?.id)];
 
     const onNavigationStateChange = (navState: any) => {
+      console.log('navState', navState);
       if (ele.id === activeTabId) {
         setTabStateMap(pre => ({
+          url: navState?.url,
           canGoBack: {
             ...pre.canGoBack,
             [ele.id]: navState?.canGoBack,
@@ -224,13 +228,13 @@ function TabsDom({ activeWebViewRef, clickBottomActionBtn }: IProps) {
               <Svg icon="more-circle" size={20} color={darkColors.iconBase1} />
             </Touchable>
             <Touchable style={rightDomStyle.inputContent} onPress={handleNaviagte}>
-              {!activeItem?.url?.includes('https://') && (
+              {!tabStateMap?.url?.includes('https://') && (
                 <View style={rightDomStyle.iconGroupWrap}>
                   <Svg icon="warning-fill" size={12} iconStyle={{ marginRight: pTd(10) }} />
                 </View>
               )}
               <TextM style={rightDomStyle.domain} numberOfLines={1}>
-                {getHost(activeItem?.url)}
+                {getHost(tabStateMap?.url)}
               </TextM>
             </Touchable>
             <Touchable onPress={() => activeWebViewRef.current?.reload?.()} style={rightDomStyle.iconWrap}>

2f382760ed57f0cb84614d57b9b077e8370fbf55
sarah-portkey <sarah@portkey.finance>
2024-11-27 19:32:23 +0800

    feat: 🎸 dapp send transaction modal

--- packages/mobile-app-did/js/dapp/components/TransactionOverlay/styles/index.ts
+++ packages/mobile-app-did/js/dapp/components/TransactionOverlay/styles/index.ts
@@ -1,96 +1,35 @@
-import { screenHeight, screenWidth } from '@portkey-wallet/utils/mobile/device';
-import { defaultColors } from 'assets/theme';
+import { makeStyles } from '@rneui/themed';
 import GStyles from 'assets/theme/GStyles';
-import fonts from 'assets/theme/fonts';
 import { StyleSheet } from 'react-native';
 import { pTd } from 'utils/unit';
 
-export const styles = StyleSheet.create({
+export const getStyles = makeStyles(theme => ({
   contentWrap: {
-    paddingLeft: pTd(20),
-    paddingRight: pTd(20),
+    ...GStyles.paddingArg(0, 16),
+    marginBottom: pTd(100),
   },
-
-  method: {
-    overflow: 'hidden',
-    borderRadius: pTd(6),
-    marginTop: pTd(24),
-    textAlign: 'center',
-    color: defaultColors.primaryColor,
-    backgroundColor: defaultColors.brandLight,
-    ...GStyles.paddingArg(2, 8),
-  },
-  transactionDataSection: {
+  authInfo: {
+    ...GStyles.paddingArg(16),
     marginTop: pTd(16),
+    height: pTd(54),
+    borderRadius: pTd(16),
+    backgroundColor: theme.colors.bgBase2,
   },
-  scrollSection: {
-    paddingLeft: pTd(20),
-    paddingRight: pTd(20),
-    width: screenWidth,
-    height: screenHeight / 2,
+  arrowIcon: {
+    transform: [{ rotate: '-90deg' }],
   },
-  blank: {
-    height: pTd(100),
-  },
-  error: {
-    color: defaultColors.error,
-    textAlign: 'left',
-    marginTop: pTd(8),
-  },
-});
-
-export const transferGroupStyle = StyleSheet.create({
-  tokenCount: {
-    marginTop: pTd(4),
-    fontSize: pTd(28),
-    textAlign: 'center',
+  bar: {
+    marginTop: pTd(12),
+    width: '100%',
+    height: pTd(1),
+    borderTopColor: theme.colors.borderBase1,
+    borderTopWidth: StyleSheet.hairlineWidth,
   },
   tokenWrap: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  loadingIcon: {
-    width: pTd(20),
-    marginRight: pTd(8),
-  },
-  smallLoadingIcon: {
-    width: pTd(12),
-    marginRight: pTd(4),
-  },
-  tokenUSD: {
-    color: defaultColors.font3,
-    textAlign: 'center',
-    marginTop: pTd(4),
-    width: pTd(335),
-  },
-  group: {
-    backgroundColor: defaultColors.bg1,
-    marginTop: pTd(24),
-    borderRadius: pTd(6),
-  },
-  tokenNum: {
-    textAlign: 'right',
-    color: defaultColors.font5,
-  },
-  usdtNum: {
-    marginLeft: pTd(6),
-    marginTop: pTd(4),
-    color: defaultColors.font3,
-    textAlign: 'right',
-  },
-  notELFWrap: {
-    height: pTd(84),
-    alignItems: 'flex-start',
-    paddingTop: pTd(18),
-    paddingBottom: pTd(18),
-  },
-  totalWithUSD: {
-    marginTop: pTd(12),
-    display: 'flex',
-    justifyContent: 'flex-end',
-    flexDirection: 'row',
   },
   flexSpaceBetween: {
     display: 'flex',
@@ -100,32 +39,11 @@ export const transferGroupStyle = StyleSheet.create({
     width: '100%',
     lineHeight: pTd(20),
   },
-  divider: {
-    marginTop: pTd(24),
-    width: pTd(335),
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: defaultColors.border6,
-  },
-  card: {
-    marginTop: pTd(24),
-    width: pTd(335),
-    borderRadius: pTd(6),
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: defaultColors.border1,
-  },
   section: {
-    ...GStyles.paddingArg(16, 12),
+    ...GStyles.paddingArg(16, 0),
   },
-  lightGrayFontColor: {
-    color: defaultColors.font3,
+  bottomText: {
+    marginTop: pTd(16),
+    color: theme.colors.textBase2,
   },
-  blackFontColor: {
-    color: defaultColors.font5,
-  },
-  fontBold: {
-    ...fonts.mediumFont,
-  },
-  marginTop0: {
-    marginTop: 0,
-  },
-});
+}));

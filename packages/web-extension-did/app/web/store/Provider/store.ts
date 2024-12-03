668a5235660a15fdca13230e4f19915280df4ef6
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-12-03 16:01:09 +0800

    fix: circular dependency

--- packages/web-extension-did/app/web/store/Provider/store.ts
+++ packages/web-extension-did/app/web/store/Provider/store.ts
@@ -4,7 +4,7 @@ import storeConfig from './config';
 import rootReducer from './rootReducer';
 import { rateApi } from '@portkey-wallet/store/rate/api';
 import { DappMiddle } from '@portkey-wallet/utils/dapp/middle';
-import SWEventController from 'controllers/SWEventController';
+// import SWEventController from 'controllers/SWEventController';
 
 export const persistedReducer = persistReducer(storeConfig.reduxPersistConfig as any, rootReducer);
 
@@ -12,7 +12,7 @@ const middlewareList: any[] = [];
 
 middlewareList.push(rateApi.middleware);
 // dapp middle
-DappMiddle.registerEvent(SWEventController);
+// DappMiddle.registerEvent(SWEventController);
 middlewareList.push(DappMiddle.middle);
 
 export const store = configureStore({

c133563a3eda01b04c3f1481a1f6aa3d6b6e5928
ptk-zz <yellow.ptk@portkey.finance>
2024-12-15 15:38:15 +0800

    refactor: 💡 switch network frame

--- packages/web-extension-did/app/web/pages/Wallet/SwitchNetwork/index.tsx
+++ packages/web-extension-did/app/web/pages/Wallet/SwitchNetwork/index.tsx
@@ -3,9 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { NetworkType } from '@portkey-wallet/types';
 import { BaseHeaderProps } from 'types/UI';
-import SwitchNetworkPrompt from './Prompt';
 import SwitchNetworkPopup from './Popup';
-import { useCommonState } from 'store/Provider/hooks';
 
 export interface Network {
   key: string;
@@ -21,13 +19,8 @@ export type ISwitchNetworkProps = BaseHeaderProps;
 export default function SwitchNetwork() {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const { isNotLessThan768 } = useCommonState();
-  const title = t('Switch Networks');
-  const goBack = useCallback(() => navigate('/setting/wallet'), [navigate]);
+  const title = t('Switch Network');
+  const goBack = useCallback(() => navigate('/setting'), [navigate]);
 
-  return isNotLessThan768 ? (
-    <SwitchNetworkPrompt headerTitle={title} goBack={goBack} />
-  ) : (
-    <SwitchNetworkPopup headerTitle={title} goBack={goBack} />
-  );
+  return <SwitchNetworkPopup headerTitle={title} goBack={goBack} />;
 }

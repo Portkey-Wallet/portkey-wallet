eeded3e8e815b6fab2638eb615a5dd0063237470
ptk-zz <yellow.ptk@portkey.finance>
2024-12-17 16:17:19 +0800

    refactor: 💡 CustomSvgV3 replace " ", "=" -> "-";

--- packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
+++ packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
@@ -19,7 +19,11 @@ export function CustomSvgV3({
   }
   return (
     <div
-      className={clsx('custom-svg-24', `${type.toLocaleLowerCase()}-icon`, className)}
+      className={clsx(
+        'custom-svg-24',
+        `${type.toLocaleLowerCase().replaceAll(' ', '-').replaceAll('=', '-')}-icon`,
+        className,
+      )}
       dangerouslySetInnerHTML={{ __html: svgContent }}
       {...props}></div>
   );

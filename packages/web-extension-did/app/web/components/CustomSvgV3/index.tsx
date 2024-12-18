ad5d570d3435e1862ab0af1abe8d68a3dc721ec9
ptk-zz <yellow.ptk@portkey.finance>
2024-12-18 23:22:08 +0800

    feat: 🎸 CustomSvgV3 support default avatars

--- packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
+++ packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
@@ -1,32 +1,61 @@
 import clsx from 'clsx';
-import { CSSProperties } from 'react';
+// import { CSSProperties } from 'react';
 import svgsList from '../../assets/iconV3/iconV3Svgs';
+import svgAvatarsList from '../../assets/iconV3/iconV3AvatarSvgs';
+
+export type SvgTypeV3 = keyof typeof svgsList;
+export type SvgAvatarTypeV3 = keyof typeof svgAvatarsList;
+
 export function CustomSvgV3({
   type,
   className,
   fillColor,
   ...props
 }: {
-  type: keyof typeof svgsList;
+  type: SvgTypeV3;
   className?: string;
   fillColor?: string;
-  style?: CSSProperties;
   onClick?: () => void;
 }) {
-  let svgContent = svgsList[type];
+  const svgContent = svgsList[type];
+  return <CustomSvgBase type={type} svgContent={svgContent} className={className} fillColor={fillColor} {...props} />;
+}
+
+export function CustomSvgAvatarV3({
+  type,
+  className,
+  fillColor,
+  ...props
+}: {
+  type: SvgAvatarTypeV3;
+  className?: string;
+  fillColor?: string;
+  onClick?: () => void;
+}) {
+  const svgContent = svgAvatarsList[type];
+  return <CustomSvgBase type={type} svgContent={svgContent} className={className} fillColor={fillColor} {...props} />;
+}
+
+function CustomSvgBase({
+  type,
+  className,
+  fillColor,
+  svgContent,
+  ...props
+}: {
+  type: string;
+  className?: string;
+  fillColor?: string;
+  onClick?: () => void;
+  svgContent: string;
+}) {
   if (fillColor) {
     svgContent = svgContent.replace(/fill="[^"]*"/g, `fill="${fillColor}"`);
   }
   return (
     <div
-      className={clsx(
-        'custom-svg-24',
-        `${type.toLocaleLowerCase().replaceAll(' ', '-').replaceAll('=', '-')}-icon`,
-        className,
-      )}
+      className={clsx('custom-svg', `${type.toLocaleLowerCase()}-icon`, className)}
       dangerouslySetInnerHTML={{ __html: svgContent }}
       {...props}></div>
   );
 }
-
-export type SvgTypeV3 = keyof typeof svgsList;

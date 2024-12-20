e90e06c21c8361e4675a131cd560576d02116586
ptk-zz <yellow.ptk@portkey.finance>
2024-12-20 15:47:15 +0800

    feat: 🎸 CustomSvgV3 add disalbed style

--- packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
+++ packages/web-extension-did/app/web/components/CustomSvgV3/index.tsx
@@ -10,30 +10,52 @@ export function CustomSvgV3({
   type,
   className,
   fillColor,
+  disabled,
   ...props
 }: {
   type: SvgTypeV3;
   className?: string;
   fillColor?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }) {
   const svgContent = svgsList[type];
-  return <CustomSvgBase type={type} svgContent={svgContent} className={className} fillColor={fillColor} {...props} />;
+  return (
+    <CustomSvgBase
+      type={type}
+      svgContent={svgContent}
+      className={className}
+      fillColor={fillColor}
+      disabled={disabled}
+      {...props}
+    />
+  );
 }
 
 export function CustomSvgAvatarV3({
   type,
   className,
   fillColor,
+  disabled,
   ...props
 }: {
   type: SvgAvatarTypeV3;
   className?: string;
   fillColor?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }) {
   const svgContent = svgAvatarsList[type];
-  return <CustomSvgBase type={type} svgContent={svgContent} className={className} fillColor={fillColor} {...props} />;
+  return (
+    <CustomSvgBase
+      type={type}
+      svgContent={svgContent}
+      className={className}
+      fillColor={fillColor}
+      disabled={disabled}
+      {...props}
+    />
+  );
 }
 
 function CustomSvgBase({
@@ -48,9 +70,13 @@ function CustomSvgBase({
   fillColor?: string;
   onClick?: () => void;
   svgContent: string;
+  disabled?: boolean;
 }) {
   if (fillColor) {
     svgContent = svgContent.replace(/fill="[^"]*"/g, `fill="${fillColor}"`);
+  }
+  if (props.disabled) {
+    svgContent = svgContent.replace(/fill="[^"]*"/g, `fill="#626264"`);
   }
   return (
     <div

6fb5535c964b1736582e899271c2ec05d0a474b6
ptk-zz <yellow.ptk@portkey.finance>
2024-12-20 22:01:22 +0800

    refactor: 💡 sige out use new modal; create useExitWallet,reuse;

--- packages/web-extension-did/app/web/pages/Wallet/components/ExitWalletModal/useExitWallet.tsx
+++ packages/web-extension-did/app/web/pages/Wallet/components/ExitWalletModal/useExitWallet.tsx
@@ -1,10 +1,5 @@
-import { Button } from 'antd';
 import { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
-import CommonModal from 'components/CommonModal';
-import './index.less';
 import { useCurrentWalletInfo, useOriginChainId } from '@portkey-wallet/hooks/hooks-ca/wallet';
-import { useLoading } from 'store/Provider/hooks';
 import { useCurrentChain } from '@portkey-wallet/hooks/hooks-ca/chainList';
 import { removeManager } from 'utils/sandboxUtil/removeManager';
 import { useCurrentNetworkInfo } from '@portkey-wallet/hooks/hooks-ca/network';
@@ -14,18 +9,11 @@ import { DEVICE_TYPE } from 'constants/index';
 import getSeed from 'utils/getSeed';
 import singleMessage from 'utils/singleMessage';
 
-interface ExitWalletModalProps {
-  open: boolean;
-  onCancel: () => void;
-}
-
-export default function ExitWalletModal({ open, onCancel }: ExitWalletModalProps) {
-  const { t } = useTranslation();
+export function useExitWallet() {
   const wallet = useCurrentWalletInfo();
   const originChainId = useOriginChainId();
 
   const currentChain = useCurrentChain(originChainId);
-  const { setLoading } = useLoading();
   const currentNetwork = useCurrentNetworkInfo();
   const logout = useLogOut();
 
@@ -33,7 +21,6 @@ export default function ExitWalletModal({ open, onCancel }: ExitWalletModalProps
     try {
       const { privateKey } = await getSeed();
       if (!currentChain?.endPoint || !privateKey) return singleMessage.error('error');
-      setLoading(true, 'Signing out of Portkey...');
       const result = await removeManager({
         rpcUrl: currentChain.endPoint,
         chainType: currentNetwork.walletType,
@@ -52,37 +39,13 @@ export default function ExitWalletModal({ open, onCancel }: ExitWalletModalProps
       });
       console.log('removeManager', 'removeManager==result', result);
       logout();
-      setLoading(false);
     } catch (error: any) {
-      setLoading(false);
       const _error = handleErrorMessage(error, 'Something error');
       singleMessage.error(_error);
     }
-  }, [currentChain, currentNetwork.walletType, logout, setLoading, wallet.address, wallet?.caHash]);
+  }, [currentChain, currentNetwork.walletType, logout, wallet.address, wallet?.caHash]);
 
-  return (
-    <CommonModal
-      className="exist-wallet"
-      closable={false}
-      width={320}
-      open={open}
-      title={t('Are you sure you want to exit your account?')}
-      footer={
-        <div className="">
-          <Button type="primary" onClick={onConfirm}>
-            {t('Exit Anyway')}
-          </Button>
-          <Button type="default" className="exist-wallet-btn-cancel" onClick={onCancel}>
-            {t('Cancel')}
-          </Button>
-        </div>
-      }>
-      <div className="text-center modal-content">
-        {/* <div className="tip-title">{t('Are you sure you want to exit your account?')}</div> */}
-        <div>
-          {t('After you exit, your assets remain in your account and you can access them through social recovery.')}
-        </div>
-      </div>
-    </CommonModal>
-  );
+  return {
+    exitWallet: onConfirm,
+  };
 }

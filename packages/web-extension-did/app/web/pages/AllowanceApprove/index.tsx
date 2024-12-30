0c453c48ee2cc48fc734e73b14e8832621bd2df8
thomas-portkey <thomas@portkey.finance>
2024-12-30 11:26:59 +0800

    feat: 🎸 discover allowance style optimization

--- packages/web-extension-did/app/web/pages/AllowanceApprove/index.tsx
+++ packages/web-extension-did/app/web/pages/AllowanceApprove/index.tsx
@@ -18,6 +18,8 @@ import getSeed from 'utils/getSeed';
 import { useDebounceCallback } from '@portkey-wallet/hooks';
 import { useCurrentNetwork } from '@portkey-wallet/hooks/hooks-ca/network';
 import './index.less';
+import { useCommonState } from 'store/Provider/hooks';
+import PortKeyHeader from 'pages/components/PortKeyHeader';
 
 export default function AllowanceApprove() {
   const { origin, chainId, icon, method, transactionInfoId, batchApproveNFT } = usePromptSearch<{
@@ -32,6 +34,8 @@ export default function AllowanceApprove() {
   const originChainId = useOriginChainId();
   const chainInfo = useCurrentChain(chainId);
   const currentNetwork = useCurrentNetwork();
+
+  const { isPrompt, isNotLessThan768 } = useCommonState();
 
   const [txParams, setTxParams] = useState<any>();
 
@@ -140,30 +144,34 @@ export default function AllowanceApprove() {
 
   return (
     <div className="manager-approve-page">
-      {txParams && (
-        <ManagerApproveInner
-          networkType={currentNetwork}
-          originChainId={originChainId}
-          spender={txParams?.params?.paramsOption?.spender}
-          targetChainId={chainId}
-          caHash={caHash || ''}
-          amount={txParams.params.paramsOption.amount}
-          symbol={txParams.params.paramsOption.symbol}
-          batchApproveNFT={batchApproveNFT}
-          dappInfo={{
-            icon,
-            href: origin,
-            name: new URL(origin).hostname,
-          }}
-          onCancel={() => {
-            closeTabPrompt(errorHandler(200003));
-          }}
-          onFinish={onFinish}
-          onError={(error) => {
-            singleMessage.error(handleErrorMessage(error));
-          }}
-        />
-      )}
+      {isPrompt && isNotLessThan768 ? <PortKeyHeader /> : <></>}
+
+      <div className="manager-approve-page-body">
+        {txParams && (
+          <ManagerApproveInner
+            networkType={currentNetwork}
+            originChainId={originChainId}
+            spender={txParams?.params?.paramsOption?.spender}
+            targetChainId={chainId}
+            caHash={caHash || ''}
+            amount={txParams.params.paramsOption.amount}
+            symbol={txParams.params.paramsOption.symbol}
+            batchApproveNFT={batchApproveNFT}
+            dappInfo={{
+              icon,
+              href: origin,
+              name: new URL(origin).hostname,
+            }}
+            onCancel={() => {
+              closeTabPrompt(errorHandler(200003));
+            }}
+            onFinish={onFinish}
+            onError={(error) => {
+              singleMessage.error(handleErrorMessage(error));
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 }

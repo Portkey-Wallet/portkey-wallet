1a9ca7201ffc3a7aa9f6fe977045b0d5c39917f2
ptk-zz <yellow.ptk@portkey.finance>
2024-12-31 15:27:57 +0800

    fix: 🐛 logic miss, only show Delete Wallet when isPrompt

--- packages/web-extension-did/app/web/pages/Wallet/WalletName/Popup/index.tsx
+++ packages/web-extension-did/app/web/pages/Wallet/WalletName/Popup/index.tsx
@@ -4,16 +4,18 @@ import { IProfileDetailProps } from 'types/Profile';
 import './index.less';
 import { useIsShowDeletion } from '@portkey-wallet/hooks/hooks-ca/account';
 import { useNavigateState } from 'hooks/router';
+import { useCommonState } from 'store/Provider/hooks';
 
 export default function WalletNamePopup({ headerTitle, goBack, data }: IProfileDetailProps) {
   const navigate = useNavigateState();
+  const { isPrompt } = useCommonState();
   const showDeletion = useIsShowDeletion();
   return (
     <div className="wallet-name-popup min-width-max-height">
       <CommonHeader title={headerTitle} onLeftBack={goBack} />
       <ViewContactBody data={data} />
 
-      {showDeletion && (
+      {showDeletion && isPrompt && (
         <div
           className="account-cancelation flex-center"
           onClick={() => navigate('/setting/wallet/account-cancelation')}>

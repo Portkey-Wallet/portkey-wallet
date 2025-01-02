8445ec50b719efc21ed706e13b11bef13ec05c5b
thomas-portkey <thomas@portkey.finance>
2025-01-02 15:17:27 +0800

    feat: 🎸 CommonPage styles optimization

--- packages/web-extension-did/app/web/components/CommonPage/index.tsx
+++ packages/web-extension-did/app/web/components/CommonPage/index.tsx
@@ -1,8 +1,9 @@
 import PortKeyHeader from 'pages/components/PortKeyHeader';
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { useCommonState } from 'store/Provider/hooks';
 import './index.less';
 import clsx from 'clsx';
+import { useLocation } from 'react-router-dom';
 
 export type TCommonPageProps = {
   children?: ReactNode;
@@ -10,10 +11,18 @@ export type TCommonPageProps = {
   contentClassName?: string;
 };
 
+const COMMON_PAGE_CLASS_NAME_MAP: Record<string, string> = {
+  '/unlock': 'large-logo-page',
+};
+
 export const CommonPage = ({ children, className, contentClassName }: TCommonPageProps) => {
+  const { pathname } = useLocation();
+
+  const extraClassName = useMemo(() => COMMON_PAGE_CLASS_NAME_MAP[pathname], [pathname]);
+
   const { isPrompt, isNotLessThan768 } = useCommonState();
   return (
-    <div className={clsx('common-page-wrap', className)}>
+    <div className={clsx('common-page-wrap', extraClassName, className)}>
       {isPrompt && isNotLessThan768 ? <PortKeyHeader /> : <></>}
       <div className={clsx('common-page-content', contentClassName)}>
         <div className="common-page-inner">{children}</div>

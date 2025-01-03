c62ad6a35238d43492ef4ba3cfb706a0bc5548e1
sarah-portkey <sarah@portkey.finance>
2025-01-03 17:21:12 +0800

    fix: 🐛 udpate send prompt

--- packages/web-extension-did/app/web/pages/Send/index.tsx
+++ packages/web-extension-did/app/web/pages/Send/index.tsx
@@ -29,10 +29,8 @@ import getTransferFee from './utils/getTransferFee';
 import { ZERO } from '@portkey-wallet/constants/misc';
 import { the2ThFailedActivityItemType } from '@portkey-wallet/types/types-ca/activity';
 import { useFetchTxFee, useGetTxFee } from '@portkey-wallet/hooks/hooks-ca/useTxFee';
-import PromptFrame from 'pages/components/PromptFrame';
 import clsx from 'clsx';
 import { IAssetToken, INftInfoType } from '@portkey-wallet/store/store-ca/assets/type';
-import PromptEmptyElement from 'pages/components/PromptEmptyElement';
 import { ChainId } from '@portkey-wallet/types';
 import { useCheckManagerSyncState } from 'hooks/wallet';
 import './index.less';
@@ -1511,7 +1509,6 @@ export default function Send() {
             buttons={modalTipContent[curModalTipKey].buttons}
           />
         )}
-        {isPrompt && <PromptEmptyElement />}
       </div>
     );
   }, [
@@ -1540,5 +1537,5 @@ export default function Send() {
     warning,
   ]);
 
-  return <>{isPrompt ? <PromptFrame content={mainContent} /> : mainContent}</>;
+  return <>{mainContent}</>;
 }

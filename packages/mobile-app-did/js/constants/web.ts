448aef516031b01c5950821917aea93e15e4f8c1
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2025-01-17 09:48:11 +0800

    fix: blob load

--- packages/mobile-app-did/js/constants/web.ts
+++ packages/mobile-app-did/js/constants/web.ts
@@ -1,2 +1,2 @@
-export const PROTOCOL_ALLOW_LIST = ['about:', 'http:', 'https:'];
+export const PROTOCOL_ALLOW_LIST = ['about:', 'http:', 'https:', 'blob:'];
 export const SCHEME_ALLOW_LIST = ['wc:', 'metamask:', 'ethereum:', 'market:'];

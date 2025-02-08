e108d24fcd6690430c2d9cb0a15346bd2b7ee2c4
ptk-zz <yellow.ptk@portkey.finance>
2025-02-08 21:18:53 +0800

    feat: 🎸 add avatar images

---
+++ packages/mobile-aelf/js/assets/image/avatars.ts
@@ -0,0 +1,23 @@
+import avatar_1 from 'assets/image/avatars/avatar_1.png';
+import avatar_2 from 'assets/image/avatars/avatar_2.png';
+import avatar_3 from 'assets/image/avatars/avatar_3.png';
+import avatar_4 from 'assets/image/avatars/avatar_4.png';
+import avatar_5 from 'assets/image/avatars/avatar_5.png';
+import avatar_6 from 'assets/image/avatars/avatar_6.png';
+import avatar_7 from 'assets/image/avatars/avatar_7.png';
+import avatar_8 from 'assets/image/avatars/avatar_8.png';
+import avatar_9 from 'assets/image/avatars/avatar_9.png';
+import avatar_10 from 'assets/image/avatars/avatar_10.png';
+
+export const LOCAL_AVATARS: { [key: string]: any } = {
+  avatar_1,
+  avatar_2,
+  avatar_3,
+  avatar_4,
+  avatar_5,
+  avatar_6,
+  avatar_7,
+  avatar_8,
+  avatar_9,
+  avatar_10,
+};

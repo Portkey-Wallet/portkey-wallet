710c8eb72517e16fdea8ab3bd59e376b0c07a619
ptk-zz <yellow.ptk@portkey.finance>
2025-02-13 11:25:35 +0800

    feat: 🎸 WalletManagement - reset app page, wallet list

--- packages/mobile-aelf/js/pages/My/WalletManagement/ResetApp/index.tsx
+++ packages/mobile-aelf/js/pages/My/WalletManagement/ResetApp/index.tsx
@@ -9,10 +9,14 @@ import { defaultColors } from 'assets/theme';
 import ActionSheet from 'components/ActionSheet';
 import Svg from 'components/Svg';
 import { useWalletCommonStyles } from '../../../Login/styles';
-import { getStyles as getWalletStyles } from '../styles';
+import { getAddressCardStyles, getStyles as getWalletStyles } from '../styles';
 import { useCheckSecurityLock } from 'hooks/securityLock';
 import { useAppCommonDispatch } from '@portkey-wallet/hooks';
+import { useWalletListState } from '@portkey-wallet/hooks/hooks-eoa/wallet';
 import { resetWallet } from '@portkey-wallet/store/store-eoa/wallet/actions';
+import CommonAvatar from 'components/CommonAvatar';
+import { LOCAL_AVATARS } from 'assets/image/avatars';
+import Touchable from 'components/Touchable';
 
 export default function ResetApp() {
   const dispatch = useAppCommonDispatch();
@@ -21,6 +25,8 @@ export default function ResetApp() {
   const checkSecurityLock = useCheckSecurityLock();
   // const credentials = useCredentials();
   const commonStyles = useWalletCommonStyles();
+  const addressCardStyles = getAddressCardStyles();
+  const walletList = useWalletListState();
 
   return (
     <View>
@@ -35,7 +41,37 @@ export default function ResetApp() {
           <Text style={[commonStyles.desc, { marginBottom: pTd(24) }]}>
             Each wallet has a seed phrase or private key, which is crucial for recovery. View and back them up:
           </Text>
-          <Text>Cards</Text>
+          {walletList.map((wallet, index) => {
+            const account = wallet.accountList[0];
+            return (
+              <View style={[addressCardStyles.card, styles.cardContainer]} key={index}>
+                <View style={addressCardStyles.info}>
+                  <CommonAvatar
+                    hasBorder={false}
+                    style={addressCardStyles.avatarIcon}
+                    localImage={LOCAL_AVATARS[account.icon || 'avatar_1']}
+                    avatarSize={pTd(24)}
+                    height={pTd(24)}
+                    width={pTd(24)}
+                  />
+                  <View>
+                    <Text style={addressCardStyles.title}>{account.name}</Text>
+                    <Text style={[addressCardStyles.subtitle, styles.subTitle]}>
+                      {wallet.AESEncryptMnemonic ? 'Seed phrase' : 'Private eky'}
+                    </Text>
+                  </View>
+                </View>
+                {/* TODO: addressManageView && notSelected */}
+                <Touchable
+                  style={styles.viewButton}
+                  onPress={() => {
+                    console.log(111);
+                  }}>
+                  <Text style={styles.viewButtonText}>View</Text>
+                </Touchable>
+              </View>
+            );
+          })}
         </ScrollView>
 
         <View style={walletStyles.deleteWalletWrap}>
@@ -108,5 +144,28 @@ const getStyles = makeStyles(theme => ({
   buttonText: {
     fontSize: pTd(16),
     ...fonts.mediumFont,
+  },
+  cardContainer: {
+    borderRadius: pTd(8),
+    backgroundColor: theme.colors.bgBase2,
+    height: pTd(72),
+    paddingHorizontal: pTd(16),
+    marginBottom: pTd(16),
+  },
+  subTitle: {
+    width: pTd(100),
+  },
+  viewButton: {
+    height: pTd(32),
+    width: pTd(66),
+    borderRadius: pTd(999),
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.textBase4,
+  },
+  viewButtonText: {
+    ...fonts.SGMediumFont,
+    color: theme.colors.bgBase1,
+    fontSize: pTd(14),
   },
 }));

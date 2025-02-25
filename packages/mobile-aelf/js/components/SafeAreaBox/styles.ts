b885c943cad57e4087d990d21c54308cad1733fc
ptk-zz <yellow.ptk@portkey.finance>
2025-02-25 17:00:44 +0800

    feat: 🎸 update SafeAreaBox bg

--- packages/mobile-aelf/js/components/SafeAreaBox/styles.ts
+++ packages/mobile-aelf/js/components/SafeAreaBox/styles.ts
@@ -1,10 +1,10 @@
 import { bottomBarHeight } from '@portkey-wallet/utils/mobile/device';
-import { defaultColors } from 'assets/theme';
+import { darkColors } from 'assets/theme';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   pageWrap: {
-    backgroundColor: defaultColors.bg1,
+    backgroundColor: darkColors.bgBase1,
     width: '100%',
     height: '100%',
   },

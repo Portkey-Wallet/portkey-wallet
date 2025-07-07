e4843b04e01341b5f708b6444f4169afd5680c97
ptk-zz <yellow.ptk@portkey.finance>
2025-07-07 18:11:16 +0800

    feat: 🎸 unlock & permission check page

--- packages/web-extension-aelf/app/web/pages/Unlock/index.tsx
+++ packages/web-extension-aelf/app/web/pages/Unlock/index.tsx
@@ -2,14 +2,11 @@ import { useCallback, useEffect } from 'react';
 import { useWalletInfo } from 'store/Provider/hooks';
 import LockPage from '../components/LockPage';
 import { useStorage } from 'hooks/useStorage';
-import { reportUserCurrentNetwork } from 'utils/analysisReport';
-import { useCurrentNetwork } from '@portkey-wallet/hooks/hooks-ca/network';
 import { useNavigateState } from 'hooks/router';
 
 const Unlock = () => {
   const navigate = useNavigateState();
-  const currentNetwork = useCurrentNetwork();
-  const { walletInfo } = useWalletInfo();
+  const { walletAddedCount } = useWalletInfo();
   const locked = useStorage('locked');
   console.log(locked, 'locked==');
   useEffect(() => {
@@ -18,19 +15,13 @@ const Unlock = () => {
     }
   }, [locked, navigate]);
 
-  useEffect(() => {
-    reportUserCurrentNetwork(currentNetwork);
-  }, [currentNetwork]);
-
   const handleNavigate = useCallback(() => {
-    const caInfo = walletInfo?.caInfo?.[currentNetwork];
-    const caHash = caInfo?.[caInfo?.originChainId || 'AELF']?.caHash;
-    if (caHash) {
+    if (walletAddedCount > 0) {
       navigate('/');
     } else {
       navigate('/register/start');
     }
-  }, [currentNetwork, navigate, walletInfo?.caInfo]);
+  }, [navigate, walletAddedCount]);
 
   return <LockPage onUnLockHandler={handleNavigate} />;
 };

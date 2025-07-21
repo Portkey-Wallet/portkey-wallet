85bfaa1aace173b9ddd17d4c51292fd4a4f1391b
ptk-zz <yellow.ptk@portkey.finance>
2025-07-21 22:03:42 +0800

    feat: 🎸 update unlock page to support check pin

--- packages/web-extension-aelf/app/web/pages/Unlock/index.tsx
+++ packages/web-extension-aelf/app/web/pages/Unlock/index.tsx
@@ -2,26 +2,48 @@ import { useCallback, useEffect } from 'react';
 import { useWalletInfo } from 'store/Provider/hooks';
 import LockPage from '../components/LockPage';
 import { useStorage } from 'hooks/useStorage';
-import { useNavigateState } from 'hooks/router';
+import { useNavigateState, useLocationState } from 'hooks/router';
+
+type TRouterParams = {
+  navigateUrl?: string;
+  params?: Record<string, any>;
+};
 
 const Unlock = () => {
   const navigate = useNavigateState();
   const { walletAddedCount } = useWalletInfo();
+  const { state } = useLocationState<TRouterParams>();
+  const { navigateUrl, params } = state || {};
   const locked = useStorage('locked');
   console.log(locked, 'locked==');
   useEffect(() => {
+    if (navigateUrl) {
+      return;
+    }
     if (locked === false) {
       return navigate('/');
     }
   }, [locked, navigate]);
 
-  const handleNavigate = useCallback(() => {
-    if (walletAddedCount > 0) {
-      navigate('/');
-    } else {
-      navigate('/register/start');
-    }
-  }, [navigate, walletAddedCount]);
+  const handleNavigate = useCallback(
+    (pwd: string) => {
+      if (navigateUrl) {
+        return navigate(navigateUrl, {
+          state:
+            {
+              ...params,
+              pin: pwd,
+            } || {},
+        });
+      }
+      if (walletAddedCount > 0) {
+        navigate('/');
+      } else {
+        navigate('/register/start');
+      }
+    },
+    [navigate, navigateUrl, params, walletAddedCount],
+  );
 
   return <LockPage onUnLockHandler={handleNavigate} />;
 };

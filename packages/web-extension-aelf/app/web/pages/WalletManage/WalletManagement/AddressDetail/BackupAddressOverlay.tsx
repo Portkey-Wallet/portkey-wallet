eaea64a6b69cfeb5c54304db577ce082ec31ea1e
ptk-zz <yellow.ptk@portkey.finance>
2025-07-24 12:02:33 +0800

    style: 💄 WalletManagement/AddressDetail/BackupAddressOverlay

--- packages/web-extension-aelf/app/web/pages/WalletManage/WalletManagement/AddressDetail/BackupAddressOverlay.tsx
+++ packages/web-extension-aelf/app/web/pages/WalletManage/WalletManagement/AddressDetail/BackupAddressOverlay.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import { DrawerOrModal } from 'components/DrawerOrModal/DrawerOrModalV2';
 import { CommonButton } from '@portkey/did-ui-react';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
 import './BackupAddressOverlay.less';
 import CheckBox from 'components/CheckBox';
+import { CustomSvgV3 } from 'components/CustomSvgV3';
 
 export type BackupType = 'seed phrase' | 'private key';
 export interface BackupAddressOverlayProps {
@@ -17,15 +17,15 @@ export interface BackupAddressOverlayProps {
 
 const notes = (type: BackupType) => [
   {
-    icon: <ExclamationCircleOutlined className="backup-note-icon" />, // 可换成自定义svg
+    icon: <CustomSvgV3 type="recovery phrase" className="backup-note-icon" />, // 可换成自定义svg
     text: `Your ${type} is essential for wallet recovery.`,
   },
   {
-    icon: <ExclamationCircleOutlined className="backup-note-icon" />,
+    icon: <CustomSvgV3 type="disabled_visible" className="backup-note-icon" />,
     text: `Losing your ${type} could mean losing access to your wallet and assets.`,
   },
   {
-    icon: <ExclamationCircleOutlined className="backup-note-icon" />,
+    icon: <CustomSvgV3 type="visibility_lock" className="backup-note-icon" />,
     text: `DO NOT share your ${type} with anyone, as this could result in wallet and asset loss.`,
   },
 ];
@@ -52,9 +52,10 @@ const BackupAddressOverlay: React.FC<BackupAddressOverlayProps> = ({
       open={open}
       onClose={onClose}
       className="backup-address-overlay-modal"
-      title={<span>Show {type}</span>}
+      title={<CustomSvgV3 type="error" className="backup-address-overlay-title-icon" />}
       content={
         <div className="backup-address-overlay-content">
+          <div className="backup-address-overlay-title">Show {type}</div>
           <div className="backup-address-overlay-notes">
             {notes(type).map((note, idx) => (
               <div className="backup-address-overlay-note" key={idx}>

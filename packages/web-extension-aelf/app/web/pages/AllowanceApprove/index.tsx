a12bcffe9adca5e9d5163d650a963efe8b4055c1
ptk-zz <yellow.ptk@portkey.finance>
2025-08-21 19:07:01 +0800

    feat: 🎸 window like popup to approve allowance

--- packages/web-extension-aelf/app/web/pages/AllowanceApprove/index.tsx
+++ packages/web-extension-aelf/app/web/pages/AllowanceApprove/index.tsx
@@ -2,7 +2,7 @@ import usePromptSearch from 'hooks/usePromptSearch';
 import singleMessage from 'utils/singleMessage';
 import { handleErrorMessage } from '@portkey-wallet/utils';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { closeTabPrompt } from 'utils/lib/serviceWorkerAction';
+import { closePrompt } from 'utils/lib/serviceWorkerAction';
 import errorHandler from 'utils/errorHandler';
 import { ExtensionContractBasic } from 'utils/sandboxUtil/ExtensionContractBasic';
 import { useCurrentChain } from '@portkey-wallet/hooks/hooks-eoa/chainList';
@@ -12,7 +12,7 @@ import { getLocalStorage } from 'utils/storage/chromeStorage';
 import { ChainId } from '@portkey-wallet/types';
 import ManagerApproveInner from './ManagerApproveInner';
 import getSeed from 'utils/getSeed';
-import { useDebounceCallback } from '@portkey-wallet/hooks';
+// import { useDebounceCallback } from '@portkey-wallet/hooks';
 import './index.less';
 import { useNetworkInfo } from 'store/Provider/hooks';
 
@@ -43,7 +43,7 @@ export default function AllowanceApprove() {
     getInitState();
   }, [getInitState]);
 
-  const onFinish = useDebounceCallback(
+  const onFinish = useCallback(
     async ({ amount, symbol }: { amount: string; symbol: string }) => {
       try {
         if (!txParams) throw Error('invalid params(txParams)');
@@ -53,14 +53,14 @@ export default function AllowanceApprove() {
         console.log(txParams, 'Token Approve==txParams====');
         console.log(chainInfo, 'Token Approve==chainInfo====');
         if (!chainInfo?.endPoint) {
-          await closeTabPrompt({
+          await closePrompt({
             ...errorHandler(400001),
             data: { code: ResponseCode.ERROR_IN_PARAMS, msg: 'invalid params' },
           });
           return;
         }
         if (chainInfo?.endPoint !== txParams?.rpcUrl) {
-          await closeTabPrompt({
+          await closePrompt({
             ...errorHandler(400001),
             data: { code: ResponseCode.ERROR_IN_PARAMS, msg: 'invalid rpcUrl' },
           });
@@ -83,24 +83,24 @@ export default function AllowanceApprove() {
           onMethod: 'transactionHash',
         });
         console.log(result, 'Token Approve==result====');
-        closeTabPrompt({
+        closePrompt({
           ...errorHandler(0),
           data: result.data,
         });
       } catch (error) {
         console.log('onFinish error', error);
-        closeTabPrompt(errorHandler(700002, handleErrorMessage(error)));
+        closePrompt(errorHandler(700002, handleErrorMessage(error)));
       }
     },
     [chainInfo, method, txParams],
-    500,
+    // 500,
   );
 
   const getTxPayload = useCallback(async () => {
     const txPayload = await getLocalStorage<{ [x: string]: any }>('txPayload');
 
     if (!txPayload[transactionInfoId]) {
-      closeTabPrompt({
+      closePrompt({
         ...errorHandler(400001),
         data: { code: ResponseCode.ERROR_IN_PARAMS },
       });
@@ -134,7 +134,7 @@ export default function AllowanceApprove() {
             name: new URL(origin).hostname,
           }}
           onCancel={() => {
-            closeTabPrompt(errorHandler(200003));
+            closePrompt(errorHandler(200003));
           }}
           onFinish={onFinish}
           onError={(error) => {
